74212405c47e48619cba40a95f23538a813bde63
Patryk Małek <patryk.malek@konghq.com>
2023-07-26 15:24:20 +0200

    chore: remove unused PodOptions (Kong/gateway-operator-archive#925)

--- apis/v1beta1/shared_types.go
+++ apis/v1beta1/shared_types.go
@@ -70,67 +70,6 @@ const (
 	BreakBeforePromotion PromotionStrategy = "BreakBeforePromotion"
 )
 
-// PodOptions is a shared type defining options on Pods deployed as part of
-// Deployments managed by the Operator.
-type PodsOptions struct {
-	// Affinity describes the scheduling rules for the pod.
-	//
-	// +optional
-	Affinity *corev1.Affinity `json:"affinity,omitempty"`
-
-	// Resources describes the compute resource requirements.
-	//
-	// +optional
-	Resources *corev1.ResourceRequirements `json:"resources,omitempty"`
-
-	// ContainerImage indicates the image that will be used for the Deployment.
-	//
-	// If omitted a default image will be automatically chosen.
-	// In case of DataPlane and ControlPlane CRDs, this is a required field,
-	// validated by the admission webhook.
-	//
-	// +optional
-	ContainerImage *string `json:"containerImage,omitempty"`
-
-	// Version indicates the desired version of the ContainerImage.
-	//
-	// Not available when AutomaticUpgrades is in use.
-	//
-	// If omitted a default version will be chosen.
-	// In case of DataPlane and ControlPlane CRDs, this is a required field,
-	// validated by the admission webhook.
-	//
-	// +optional
-	Version *string `json:"version,omitempty"`
-
-	// Env indicates the environment variables to set for the Deployment.
-	//
-	// +optional
-	Env []corev1.EnvVar `json:"env,omitempty"`
-
-	// EnvFrom indicates the environment variables to be set for the Deployment
-	// with the values set from specific sources (such as Secrets).
-	//
-	// +optional
-	EnvFrom []corev1.EnvFromSource `json:"envFrom,omitempty"`
-
-	// Volumes defines additional volumes to attach to the pods in the Deployment.
-	//
-	// +optional
-	Volumes []corev1.Volume `json:"volumes,omitempty"`
-
-	// VolumeMounts defines additional volumes to mount to proxy containers of pods
-	// in the Deployment.
-	//
-	// +optional
-	VolumeMounts []corev1.VolumeMount `json:"volumeMounts,omitempty"`
-
-	// Labels define labels on Deployment's pods.
-	//
-	// +optional
-	Labels map[string]string `json:"labels,omitempty"`
-}
-
 // GatewayConfigurationTargetKind is an object kind that can be targeted for
 // GatewayConfiguration attachment.
 type GatewayConfigurationTargetKind string
