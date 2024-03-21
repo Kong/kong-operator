db149e5c8b3a088a35dadb7f86fcc422dd27e4a5
Patryk Małek <patryk.malek@konghq.com>
2024-03-21 13:48:27 +0100

    chore: do not report an error when reducing resources receives a NotFound error

--- pkg/utils/kubernetes/reduce/reduce.go
+++ pkg/utils/kubernetes/reduce/reduce.go
@@ -32,7 +32,7 @@ func ReduceSecrets(ctx context.Context, k8sClient client.Client, secrets []corev
 				return fmt.Errorf("failed to execute pre delete hook: %w", err)
 			}
 		}
-		if err := k8sClient.Delete(ctx, &secret); err != nil {
+		if err := k8sClient.Delete(ctx, &secret); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
@@ -46,7 +46,7 @@ func ReduceServiceAccounts(ctx context.Context, k8sClient client.Client, service
 	filteredServiceAccounts := filterServiceAccounts(serviceAccounts)
 	for _, serviceAccount := range filteredServiceAccounts {
 		serviceAccount := serviceAccount
-		if err := k8sClient.Delete(ctx, &serviceAccount); err != nil {
+		if err := k8sClient.Delete(ctx, &serviceAccount); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
@@ -60,7 +60,7 @@ func ReduceClusterRoles(ctx context.Context, k8sClient client.Client, clusterRol
 	filteredClusterRoles := filterClusterRoles(clusterRoles)
 	for _, clusterRole := range filteredClusterRoles {
 		clusterRole := clusterRole
-		if err := k8sClient.Delete(ctx, &clusterRole); err != nil {
+		if err := k8sClient.Delete(ctx, &clusterRole); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
@@ -74,7 +74,7 @@ func ReduceClusterRoleBindings(ctx context.Context, k8sClient client.Client, clu
 	filteredCLusterRoleBindings := filterClusterRoleBindings(clusterRoleBindings)
 	for _, clusterRoleBinding := range filteredCLusterRoleBindings {
 		clusterRoleBinding := clusterRoleBinding
-		if err := k8sClient.Delete(ctx, &clusterRoleBinding); err != nil {
+		if err := k8sClient.Delete(ctx, &clusterRoleBinding); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
@@ -94,7 +94,7 @@ func ReduceDeployments(ctx context.Context, k8sClient client.Client, deployments
 				return fmt.Errorf("failed to execute pre delete hook: %w", err)
 			}
 		}
-		if err := k8sClient.Delete(ctx, &deployment); err != nil {
+		if err := k8sClient.Delete(ctx, &deployment); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
@@ -128,7 +128,7 @@ func ReduceServices(ctx context.Context, k8sClient client.Client, services []cor
 				return fmt.Errorf("failed to execute pre delete hook: %w", err)
 			}
 		}
-		if err := k8sClient.Delete(ctx, &service); err != nil {
+		if err := k8sClient.Delete(ctx, &service); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
@@ -142,7 +142,7 @@ func ReduceNetworkPolicies(ctx context.Context, k8sClient client.Client, network
 	filteredNetworkPolicies := filterNetworkPolicies(networkPolicies)
 	for _, networkPolicy := range filteredNetworkPolicies {
 		networkPolicy := networkPolicy
-		if err := k8sClient.Delete(ctx, &networkPolicy); err != nil {
+		if err := k8sClient.Delete(ctx, &networkPolicy); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
@@ -158,7 +158,7 @@ type HPAFilterFunc func(hpas []autoscalingv2.HorizontalPodAutoscaler) []autoscal
 func ReduceHPAs(ctx context.Context, k8sClient client.Client, hpas []autoscalingv2.HorizontalPodAutoscaler, filter HPAFilterFunc) error {
 	for _, hpa := range filter(hpas) {
 		hpa := hpa
-		if err := k8sClient.Delete(ctx, &hpa); err != nil {
+		if err := k8sClient.Delete(ctx, &hpa); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
@@ -172,7 +172,7 @@ func ReduceValidatingWebhookConfigurations(ctx context.Context, k8sClient client
 	filteredWebhookConfigurations := filterValidatingWebhookConfigurations(webhookConfigurations)
 	for _, webhookConfiguration := range filteredWebhookConfigurations {
 		webhookConfiguration := webhookConfiguration
-		if err := k8sClient.Delete(ctx, &webhookConfiguration); err != nil {
+		if err := k8sClient.Delete(ctx, &webhookConfiguration); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
@@ -186,7 +186,7 @@ func ReduceDataPlanes(ctx context.Context, k8sClient client.Client, dataplanes [
 	filteredDataPlanes := filterDataPlanes(dataplanes)
 	for _, dataplane := range filteredDataPlanes {
 		dataplane := dataplane
-		if err := k8sClient.Delete(ctx, &dataplane); err != nil {
+		if err := k8sClient.Delete(ctx, &dataplane); client.IgnoreNotFound(err) != nil {
 			return err
 		}
 	}
