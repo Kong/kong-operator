hiroshinishio/gateway-operator

feat: dataplaneKonnectExtension CRD
Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>

api/v1alpha1/zz_generated.deepcopy.go
@@ -261,6 +261,107 @@ func (in *CloudHostedLargeLanguageModel) DeepCopy() *CloudHostedLargeLanguageMod
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *DataPlaneKonnectExtension) DeepCopyInto(out *DataPlaneKonnectExtension) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new DataPlaneKonnectExtension.
+func (in *DataPlaneKonnectExtension) DeepCopy() *DataPlaneKonnectExtension {
+	if in == nil {
+		return nil
+	}
+	out := new(DataPlaneKonnectExtension)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *DataPlaneKonnectExtension) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *DataPlaneKonnectExtensionList) DeepCopyInto(out *DataPlaneKonnectExtensionList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]DataPlaneKonnectExtension, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new DataPlaneKonnectExtensionList.
+func (in *DataPlaneKonnectExtensionList) DeepCopy() *DataPlaneKonnectExtensionList {
+	if in == nil {
+		return nil
+	}
+	out := new(DataPlaneKonnectExtensionList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *DataPlaneKonnectExtensionList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *DataPlaneKonnectExtensionSpec) DeepCopyInto(out *DataPlaneKonnectExtensionSpec) {
+	*out = *in
+	if in.ControlPlaneRegion != nil {
+		in, out := &in.ControlPlaneRegion, &out.ControlPlaneRegion
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new DataPlaneKonnectExtensionSpec.
+func (in *DataPlaneKonnectExtensionSpec) DeepCopy() *DataPlaneKonnectExtensionSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(DataPlaneKonnectExtensionSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *DataPlaneKonnectExtensionStatus) DeepCopyInto(out *DataPlaneKonnectExtensionStatus) {
+	*out = *in
+	if in.DataPlaneRefs != nil {
+		in, out := &in.DataPlaneRefs, &out.DataPlaneRefs
+		*out = make([]NamespacedRef, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new DataPlaneKonnectExtensionStatus.
+func (in *DataPlaneKonnectExtensionStatus) DeepCopy() *DataPlaneKonnectExtensionStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(DataPlaneKonnectExtensionStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *DataPlaneMetricsExtension) DeepCopyInto(out *DataPlaneMetricsExtension) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta

Kong/kong-operator

api/v1alpha1/zz_generated.deepcopy.go
@@ -261,6 +261,110 @@ func (in *CloudHostedLargeLanguageModel) DeepCopy() *CloudHostedLargeLanguageMod
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *DataPlaneKonnectExtension) DeepCopyInto(out *DataPlaneKonnectExtension) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new DataPlaneKonnectExtension.
+func (in *DataPlaneKonnectExtension) DeepCopy() *DataPlaneKonnectExtension {
+	if in == nil {
+		return nil
+	}
+	out := new(DataPlaneKonnectExtension)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *DataPlaneKonnectExtension) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *DataPlaneKonnectExtensionList) DeepCopyInto(out *DataPlaneKonnectExtensionList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]DataPlaneKonnectExtension, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new DataPlaneKonnectExtensionList.
+func (in *DataPlaneKonnectExtensionList) DeepCopy() *DataPlaneKonnectExtensionList {
+	if in == nil {
+		return nil
+	}
+	out := new(DataPlaneKonnectExtensionList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *DataPlaneKonnectExtensionList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *DataPlaneKonnectExtensionSpec) DeepCopyInto(out *DataPlaneKonnectExtensionSpec) {
+	*out = *in
+	in.ControlPlaneRef.DeepCopyInto(&out.ControlPlaneRef)
+	if in.ClusterDataPlaneLabels != nil {
+		in, out := &in.ClusterDataPlaneLabels, &out.ClusterDataPlaneLabels
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new DataPlaneKonnectExtensionSpec.
+func (in *DataPlaneKonnectExtensionSpec) DeepCopy() *DataPlaneKonnectExtensionSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(DataPlaneKonnectExtensionSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *DataPlaneKonnectExtensionStatus) DeepCopyInto(out *DataPlaneKonnectExtensionStatus) {
+	*out = *in
+	if in.DataPlaneRefs != nil {
+		in, out := &in.DataPlaneRefs, &out.DataPlaneRefs
+		*out = make([]NamespacedRef, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new DataPlaneKonnectExtensionStatus.
+func (in *DataPlaneKonnectExtensionStatus) DeepCopy() *DataPlaneKonnectExtensionStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(DataPlaneKonnectExtensionStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *DataPlaneMetricsExtension) DeepCopyInto(out *DataPlaneMetricsExtension) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta