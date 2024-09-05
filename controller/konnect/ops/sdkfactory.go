0ace883118c7f61f8a0ccae1518721d0845ef9f6
Tao Yi <tao.yi@konghq.com>
2024-09-05 21:31:48 +0800

    test(konnect): Add SDKWrapper to allow using mock SDK in reconcilers (#543)
    * refactor(konnect): add SDK interface types for each entity type
    
    * tests: add mockery for Konnect SDK
    
    * tests: add mocks for KongService SDK
    
    * add plugin SDK interface and mock
    
    * refactor SDKFactory to allow using mock SDK in reconcilers
    
    * add mocks for all submodules of Konnect SDK
    
    * move mock SDK code to *_mock.go to allow them being imported by other packages
    
    * Update controller/konnect/reconciler_generic_test.go
    
    Co-authored-by: Grzegorz Burzyński <czeslavo@gmail.com>
    
    ---------
    
    Co-authored-by: Patryk Małek <patryk.malek@konghq.com>
    Co-authored-by: Grzegorz Burzyński <czeslavo@gmail.com>

---
+++ controller/konnect/ops/sdkfactory.go
@@ -0,0 +1,87 @@
+package ops
+
+import (
+	sdkkonnectgo "github.com/Kong/sdk-konnect-go"
+	sdkkonnectcomp "github.com/Kong/sdk-konnect-go/models/components"
+)
+
+// SDKWrapper is a wrapper of Konnect SDK to allow using mock SDKs in tests.
+type SDKWrapper interface {
+	GetControlPlaneSDK() ControlPlaneSDK
+	GetServicesSDK() ServicesSDK
+	GetRoutesSDK() RoutesSDK
+	GetConsumersSDK() ConsumersSDK
+	GetConsumerGroupsSDK() ConsumerGroupSDK
+	GetPluginSDK() PluginSDK
+	GetMeSDK() MeSDK
+}
+
+type sdkWrapper struct {
+	sdk *sdkkonnectgo.SDK
+}
+
+var _ SDKWrapper = sdkWrapper{}
+
+// GetControlPlaneSDK returns the SDK to operate Konenct control planes.
+func (w sdkWrapper) GetControlPlaneSDK() ControlPlaneSDK {
+	return w.sdk.ControlPlanes
+}
+
+// GetServicesSDK returns the SDK to operate Kong services.
+func (w sdkWrapper) GetServicesSDK() ServicesSDK {
+	return w.sdk.Services
+}
+
+// GetRoutesSDK returns the SDK to operate Kong routes.
+func (w sdkWrapper) GetRoutesSDK() RoutesSDK {
+	return w.sdk.Routes
+}
+
+// GetConsumersSDK returns the SDK to operate Kong consumers.
+func (w sdkWrapper) GetConsumersSDK() ConsumersSDK {
+	return w.sdk.Consumers
+}
+
+// GetConsumerGroupsSDK returns the SDK to operate Kong consumer groups.
+func (w sdkWrapper) GetConsumerGroupsSDK() ConsumerGroupSDK {
+	return w.sdk.ConsumerGroups
+}
+
+// GetPluginSDK returns the SDK to operate plugins.
+func (w sdkWrapper) GetPluginSDK() PluginSDK {
+	return w.sdk.Plugins
+}
+
+// GetMeSDK returns the "me" SDK to get current organization.
+func (w sdkWrapper) GetMeSDK() MeSDK {
+	return w.sdk.Me
+}
+
+// SDKToken is a token used to authenticate with the Konnect SDK.
+type SDKToken string
+
+// SDKFactory is a factory for creating Konnect SDKs.
+type SDKFactory interface {
+	NewKonnectSDK(serverURL string, token SDKToken) SDKWrapper
+}
+
+type sdkFactory struct{}
+
+// NewSDKFactory creates a new SDKFactory.
+func NewSDKFactory() SDKFactory {
+	return sdkFactory{}
+}
+
+// NewKonnectSDK creates a new Konnect SDK.
+func (f sdkFactory) NewKonnectSDK(serverURL string, token SDKToken) SDKWrapper {
+	return sdkWrapper{
+		sdk: sdkkonnectgo.New(
+			sdkkonnectgo.WithSecurity(
+				sdkkonnectcomp.Security{
+					PersonalAccessToken: sdkkonnectgo.String(string(token)),
+				},
+			),
+			sdkkonnectgo.WithServerURL(serverURL),
+		),
+	}
+}
