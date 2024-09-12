7f30d59543ef455e9880dd6c7202c6048c36ec78
Tao Yi <richardyi0110@hotmail.com>
2024-09-12 15:35:46 +0800

    add Konnect related fields and methods to KongVault (#66)

--- api/configuration/v1alpha1/kong_vault_types.go
+++ api/configuration/v1alpha1/kong_vault_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	konnectv1alpha1 "github.com/kong/kubernetes-configuration/api/konnect/v1alpha1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -49,6 +50,58 @@ type KongVault struct {
 	Status            KongVaultStatus `json:"status,omitempty"`
 }
 
+// initKonnectStatus initializes the Konnect status of KongVualt when it is empty.
+func (v *KongVault) initKonnectStatus() {
+	v.Status.Konnect = &konnectv1alpha1.KonnectEntityStatusWithControlPlaneRef{}
+}
+
+// GetKonnectStatus returns the Konnect status contained in the KongVault status.
+func (v *KongVault) GetKonnectStatus() *konnectv1alpha1.KonnectEntityStatus {
+	if v.Status.Konnect == nil {
+		return nil
+	}
+	return &v.Status.Konnect.KonnectEntityStatus
+}
+
+// GetKonnectID returns the Konnect ID in the KongVault status.
+func (v *KongVault) GetKonnectID() string {
+	if v.Status.Konnect == nil {
+		return ""
+	}
+	return v.Status.Konnect.ID
+}
+
+// SetKonnectID sets the Konnect ID in the KongVault status.
+func (v *KongVault) SetKonnectID(id string) {
+	if v.Status.Konnect == nil {
+		v.initKonnectStatus()
+	}
+	v.Status.Konnect.ID = id
+}
+
+// SetControlPlaneID sets the ControlPlane ID in the KongVault status.
+func (v *KongVault) SetControlPlaneID(id string) {
+	if v.Status.Konnect == nil {
+		v.initKonnectStatus()
+	}
+	v.Status.Konnect.ControlPlaneID = id
+}
+
+// GetTypeName returns the KongVault Kind name
+func (v KongVault) GetTypeName() string {
+	return "KongVault"
+}
+
+// GetConditions returns the Status Conditions in the KongVault.
+func (v *KongVault) GetConditions() []metav1.Condition {
+	return v.Status.Conditions
+}
+
+// SetConditions sets the Status Conditions in the KongVault.
+func (v *KongVault) SetConditions(conditions []metav1.Condition) {
+	v.Status.Conditions = conditions
+}
+
 // KongVaultSpec defines specification of a custom Kong vault.
 type KongVaultSpec struct {
 	// Backend is the type of the backend storing the secrets in the vault.
@@ -64,10 +117,19 @@ type KongVaultSpec struct {
 	Description string `json:"description,omitempty"`
 	// Config is the configuration of the vault. Varies for different backends.
 	Config apiextensionsv1.JSON `json:"config,omitempty"`
+	// Tags are the tags associated to the vault for grouping and filtering.
+	Tags []string `json:"tags,omitempty"`
+	// ControlPlaneRef is a reference to a Konnect ControlPlane this KongVault is associated with.
+	// +optional
+	ControlPlaneRef *ControlPlaneRef `json:"controlPlaneRef,omitempty"`
 }
 
 // KongVaultStatus represents the current status of the KongVault resource.
 type KongVaultStatus struct {
+	// Konnect contains the Konnect entity status.
+	// +optional
+	Konnect *konnectv1alpha1.KonnectEntityStatusWithControlPlaneRef `json:"konnect,omitempty"`
+
 	// Conditions describe the current conditions of the KongVaultStatus.
 	//
 	// Known condition types are:
