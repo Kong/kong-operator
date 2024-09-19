c013e7fc54a0235093a4752de1621149ca5c3121
Patryk Małek <patryk.malek@konghq.com>
2024-09-19 10:07:50 +0200

    test: refactor Konnect funcs unit test (#86)

---
+++ api/test/unit/konnect_funcs_test.go
@@ -0,0 +1,73 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	configurationv1 "github.com/kong/kubernetes-configuration/api/configuration/v1"
+	configurationv1alpha1 "github.com/kong/kubernetes-configuration/api/configuration/v1alpha1"
+	configurationv1beta1 "github.com/kong/kubernetes-configuration/api/configuration/v1beta1"
+	konnectv1alpha1 "github.com/kong/kubernetes-configuration/api/konnect/v1alpha1"
+)
+
+func TestKonnectFuncs(t *testing.T) {
+	type KonnectEntity interface {
+		client.Object
+		GetTypeName() string
+		SetControlPlaneID(string)
+		GetControlPlaneID() string
+		GetKonnectStatus() *konnectv1alpha1.KonnectEntityStatus
+	}
+
+	testcases := []struct {
+		object   KonnectEntity
+		typeName string
+	}{
+		{
+			typeName: "KongConsumer",
+			object:   &configurationv1.KongConsumer{},
+		},
+		{
+			typeName: "KongConsumerGroup",
+			object:   &configurationv1beta1.KongConsumerGroup{},
+		},
+		{
+			typeName: "KongPluginBinding",
+			object:   &configurationv1alpha1.KongPluginBinding{},
+		},
+		{
+			typeName: "KongUpstream",
+			object:   &configurationv1alpha1.KongUpstream{},
+		},
+		{
+			typeName: "KongTarget",
+			object:   &configurationv1alpha1.KongTarget{},
+		},
+		{
+			typeName: "KongService",
+			object:   &configurationv1alpha1.KongService{},
+		},
+		{
+			typeName: "KongVault",
+			object:   &configurationv1alpha1.KongVault{},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.object.GetTypeName(), func(t *testing.T) {
+			obj := tc.object
+
+			require.Equal(t, obj.GetTypeName(), tc.typeName)
+			require.Nil(t, obj.GetKonnectStatus())
+			require.Empty(t, obj.GetKonnectStatus().GetKonnectID())
+			require.Empty(t, obj.GetKonnectStatus().GetOrgID())
+			require.Empty(t, obj.GetKonnectStatus().GetServerURL())
+
+			require.Equal(t, "", obj.GetControlPlaneID())
+			obj.SetControlPlaneID("123")
+			require.Equal(t, "123", obj.GetControlPlaneID())
+		})
+	}
+}
