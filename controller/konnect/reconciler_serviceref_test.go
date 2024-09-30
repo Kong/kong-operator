a6f81c06a129a830b8479f8ff21babb3073eefb1
Patryk Małek <patryk.malek@konghq.com>
2024-09-30 11:35:51 +0200

    tests: add serviceref tests

---
+++ controller/konnect/reconciler_serviceref_test.go
@@ -0,0 +1,321 @@
+package konnect
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/kong/gateway-operator/controller/konnect/conditions"
+	"github.com/kong/gateway-operator/controller/konnect/constraints"
+
+	configurationv1alpha1 "github.com/kong/kubernetes-configuration/api/configuration/v1alpha1"
+	konnectv1alpha1 "github.com/kong/kubernetes-configuration/api/konnect/v1alpha1"
+)
+
+type handleServiceRefTestCase[T constraints.SupportedKonnectEntityType, TEnt constraints.EntityType[T]] struct {
+	name                string
+	ent                 TEnt
+	objects             []client.Object
+	expectResult        ctrl.Result
+	expectError         bool
+	expectErrorContains string
+	// Returns true if the updated entity satisfy the assertion.
+	// Returns false and error message if entity fails to satisfy it.
+	updatedEntAssertions []func(TEnt) (ok bool, message string)
+}
+
+var testKongServiceOK = &configurationv1alpha1.KongService{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "svc-ok",
+		Namespace: "default",
+	},
+	Spec: configurationv1alpha1.KongServiceSpec{
+		ControlPlaneRef: &configurationv1alpha1.ControlPlaneRef{
+			Type: configurationv1alpha1.ControlPlaneRefKonnectNamespacedRef,
+			KonnectNamespacedRef: &configurationv1alpha1.KonnectNamespacedRef{
+				Name: "cp-ok",
+			},
+		},
+	},
+	Status: configurationv1alpha1.KongServiceStatus{
+		Konnect: &konnectv1alpha1.KonnectEntityStatusWithControlPlaneRef{
+			KonnectEntityStatus: konnectv1alpha1.KonnectEntityStatus{
+				ID: "12345",
+			},
+			ControlPlaneID: "123456789",
+		},
+		Conditions: []metav1.Condition{
+			{
+				Type:   conditions.KonnectEntityProgrammedConditionType,
+				Status: metav1.ConditionTrue,
+			},
+		},
+	},
+}
+
+var testKongServiceWithCPRefUnprogrammed = &configurationv1alpha1.KongService{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "svc-with-cp-ref-unprogrammed",
+		Namespace: "default",
+	},
+	Spec: configurationv1alpha1.KongServiceSpec{
+		ControlPlaneRef: &configurationv1alpha1.ControlPlaneRef{
+			Type: configurationv1alpha1.ControlPlaneRefKonnectNamespacedRef,
+			KonnectNamespacedRef: &configurationv1alpha1.KonnectNamespacedRef{
+				Name: "cp-not-programmed",
+			},
+		},
+	},
+	Status: configurationv1alpha1.KongServiceStatus{
+		Konnect: &konnectv1alpha1.KonnectEntityStatusWithControlPlaneRef{
+			KonnectEntityStatus: konnectv1alpha1.KonnectEntityStatus{
+				ID: "12345",
+			},
+			ControlPlaneID: "123456789",
+		},
+		Conditions: []metav1.Condition{
+			{
+				Type:   conditions.KonnectEntityProgrammedConditionType,
+				Status: metav1.ConditionTrue,
+			},
+		},
+	},
+}
+
+var testKongServiceNotProgrammed = &configurationv1alpha1.KongService{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "svc-not-programmed",
+		Namespace: "default",
+	},
+	Status: configurationv1alpha1.KongServiceStatus{
+		Conditions: []metav1.Condition{
+			{
+				Type:   conditions.KonnectEntityProgrammedConditionType,
+				Status: metav1.ConditionFalse,
+			},
+		},
+	},
+}
+
+var testKongServiceBeingDeleted = &configurationv1alpha1.KongService{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:              "svc-being-deleted",
+		Namespace:         "default",
+		DeletionTimestamp: &metav1.Time{Time: time.Now()},
+		Finalizers:        []string{KonnectCleanupFinalizer},
+	},
+}
+
+func TestHandleServiceRef(t *testing.T) {
+	testCases := []handleServiceRefTestCase[configurationv1alpha1.KongRoute, *configurationv1alpha1.KongRoute]{
+		{
+			name: "has service ref",
+			ent: &configurationv1alpha1.KongRoute{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "route-1",
+					Namespace: "default",
+				},
+				Spec: configurationv1alpha1.KongRouteSpec{
+					ServiceRef: &configurationv1alpha1.ServiceRef{
+						Type: configurationv1alpha1.ServiceRefNamespacedRef,
+						NamespacedRef: &configurationv1alpha1.NamespacedServiceRef{
+							Name: "svc-ok",
+						},
+					},
+				},
+			},
+			objects: []client.Object{
+				testKongServiceOK,
+				testControlPlaneOK,
+			},
+			expectResult: ctrl.Result{},
+			expectError:  false,
+			updatedEntAssertions: []func(*configurationv1alpha1.KongRoute) (bool, string){
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return lo.ContainsBy(ks.Status.Conditions, func(c metav1.Condition) bool {
+						return c.Type == conditions.ControlPlaneRefValidConditionType && c.Status == metav1.ConditionTrue
+					}), "KongRoute does not have ControlPlaneRefValid condition set to True"
+				},
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return lo.ContainsBy(ks.Status.Conditions, func(c metav1.Condition) bool {
+						return c.Type == conditions.KongServiceRefValidConditionType && c.Status == metav1.ConditionTrue
+					}), "KongRoute does not have KongServiceRefValid condition set to True"
+				},
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return lo.ContainsBy(ks.OwnerReferences, func(o metav1.OwnerReference) bool {
+						return o.Kind == "KongService" && o.Name == "svc-ok"
+					}), "OwnerReference of KongRoute is not set"
+				},
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return ks.Status.Konnect.ServiceID == "12345",
+						"KongRoute does not have Konnect Service ID set"
+				},
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return ks.Status.Konnect.ControlPlaneID == "123456789",
+						"KongRoute does not have Konnect ControlPlane ID set (should be inherited from ControlPlane)"
+				},
+			},
+		},
+		{
+			name: "with service ref to a service that is being deleted",
+			ent: &configurationv1alpha1.KongRoute{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "route-with-service-ref-being-deleted",
+					Namespace: "default",
+				},
+				Spec: configurationv1alpha1.KongRouteSpec{
+					ServiceRef: &configurationv1alpha1.ServiceRef{
+						Type: configurationv1alpha1.ServiceRefNamespacedRef,
+						NamespacedRef: &configurationv1alpha1.NamespacedServiceRef{
+							Name: "svc-ok",
+						},
+					},
+				},
+			},
+			objects: []client.Object{
+				testKongServiceBeingDeleted,
+				testControlPlaneOK,
+			},
+			expectResult: ctrl.Result{},
+			expectError:  true,
+		},
+		{
+			name: "has service ref to an unprogrammed service",
+			ent: &configurationv1alpha1.KongRoute{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "route-1",
+					Namespace: "default",
+				},
+				Spec: configurationv1alpha1.KongRouteSpec{
+					ServiceRef: &configurationv1alpha1.ServiceRef{
+						Type: configurationv1alpha1.ServiceRefNamespacedRef,
+						NamespacedRef: &configurationv1alpha1.NamespacedServiceRef{
+							Name: "svc-not-programmed",
+						},
+					},
+				},
+			},
+			objects: []client.Object{
+				testKongServiceNotProgrammed,
+				testControlPlaneOK,
+			},
+			expectResult: ctrl.Result{
+				Requeue: true,
+			},
+			expectError: false,
+			updatedEntAssertions: []func(*configurationv1alpha1.KongRoute) (bool, string){
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return lo.ContainsBy(ks.Status.Conditions, func(c metav1.Condition) bool {
+						return c.Type == conditions.KongServiceRefValidConditionType &&
+							c.Status == metav1.ConditionFalse &&
+							c.Reason == conditions.KongServiceRefReasonInvalid
+					}), "KongRoute does not have KongServiceRefValid condition set to False"
+				},
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return lo.NoneBy(ks.OwnerReferences, func(o metav1.OwnerReference) bool {
+						return o.Kind == "KongService" && o.Name == "svc-ok"
+					}), "OwnerReference of KongRoute is set but it shouldn't be"
+				},
+			},
+		},
+		{
+			name: "has service ref which has an unprogrammed cp",
+			ent: &configurationv1alpha1.KongRoute{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "route-1",
+					Namespace: "default",
+				},
+				Spec: configurationv1alpha1.KongRouteSpec{
+					ServiceRef: &configurationv1alpha1.ServiceRef{
+						Type: configurationv1alpha1.ServiceRefNamespacedRef,
+						NamespacedRef: &configurationv1alpha1.NamespacedServiceRef{
+							Name: "svc-with-cp-ref-unprogrammed",
+						},
+					},
+				},
+			},
+			objects: []client.Object{
+				testKongServiceWithCPRefUnprogrammed,
+				testControlPlaneNotProgrammed,
+			},
+			expectResult: ctrl.Result{
+				Requeue: true,
+			},
+			expectError: false,
+			updatedEntAssertions: []func(*configurationv1alpha1.KongRoute) (bool, string){
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return lo.ContainsBy(ks.Status.Conditions, func(c metav1.Condition) bool {
+						return c.Type == conditions.ControlPlaneRefValidConditionType &&
+							c.Status == metav1.ConditionFalse &&
+							c.Reason == conditions.ControlPlaneRefReasonInvalid
+					}), "KongRoute does not have ControlPlaneRef condition set to False"
+				},
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return lo.ContainsBy(ks.Status.Conditions, func(c metav1.Condition) bool {
+						return c.Type == conditions.KongServiceRefValidConditionType &&
+							c.Status == metav1.ConditionTrue &&
+							c.Reason == conditions.KongServiceRefReasonValid
+					}), "KongRoute does not have KongServiceRefValid condition set to True"
+				},
+				func(ks *configurationv1alpha1.KongRoute) (bool, string) {
+					return lo.NoneBy(ks.OwnerReferences, func(o metav1.OwnerReference) bool {
+						return o.Kind == "KongService" && o.Name == "svc-ok"
+					}), "OwnerReference of KongRoute is set but it shouldn't be"
+				},
+			},
+		},
+	}
+
+	testHandleServiceRef(t, testCases)
+}
+
+func testHandleServiceRef[
+	T constraints.SupportedKonnectEntityType,
+	TEnt constraints.EntityType[T],
+](
+	t *testing.T, testCases []handleServiceRefTestCase[T, TEnt],
+) {
+	t.Helper()
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			scheme := runtime.NewScheme()
+			require.NoError(t, configurationv1alpha1.AddToScheme(scheme))
+			require.NoError(t, konnectv1alpha1.AddToScheme(scheme))
+			fakeClient := fake.NewClientBuilder().
+				WithScheme(scheme).
+				WithObjects(tc.ent).
+				WithObjects(tc.objects...).
+				// WithStatusSubresource is required for updating status of handled entity.
+				WithStatusSubresource(tc.ent).
+				Build()
+			require.NoError(t, fakeClient.SubResource("status").Update(context.Background(), tc.ent))
+
+			res, err := handleKongServiceRef(context.Background(), fakeClient, tc.ent)
+
+			var updatedEnt TEnt = tc.ent.DeepCopyObject().(TEnt)
+			require.NoError(t, fakeClient.Get(context.Background(), client.ObjectKeyFromObject(tc.ent), updatedEnt))
+			for _, assertion := range tc.updatedEntAssertions {
+				ok, msg := assertion(updatedEnt)
+				require.True(t, ok, msg)
+			}
+
+			if tc.expectError {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.expectErrorContains)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.expectResult, res)
+		})
+	}
+}
