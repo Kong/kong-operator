32c5c31d0dcacf018278b8bf3ff314b6b82d3a8e
Patryk Małek <patryk.malek@konghq.com>
2024-10-02 14:00:14 +0200

    feat(konnect): manage finalizers for KongRoute in KonnectEntityPluginBindingFinalizerReconciler

--- controller/konnect/reconciler_generic_pluginbindingfinalizer_rbac.go
+++ controller/konnect/reconciler_generic_pluginbindingfinalizer_rbac.go
@@ -2,4 +2,6 @@ package konnect
 
 //+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongservices,verbs=get;update
 
+//+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongroutes,verbs=get;update
+
 //+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongpluginbindings,verbs=delete;list
