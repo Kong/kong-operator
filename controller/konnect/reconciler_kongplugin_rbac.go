0480a5075b6581c0c909e2c8641a55e9834a4056
Patryk Małek <patryk.malek@konghq.com>
2024-10-10 16:08:27 +0200

    fix: add missing RBAC policy rules for Konnect entities (#735)

--- controller/konnect/reconciler_kongplugin_rbac.go
+++ controller/konnect/reconciler_kongplugin_rbac.go
@@ -1,5 +1,8 @@
 package konnect
 
 //+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongplugins,verbs=get;list;watch;update;patch
+//+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongconsumers,verbs=get;list;watch;update;patch
+//+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongconsumergroups,verbs=get;list;watch;update;patch
 //+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongservices,verbs=get;list;watch;update;patch
-//+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongpluginbindings,verbs=get;list;create;watch;update;patch
+//+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongroutes,verbs=get;list;watch;update;patch
+//+kubebuilder:rbac:groups=configuration.konghq.com,resources=kongpluginbindings,verbs=get;list;create;watch;update;patch;delete
