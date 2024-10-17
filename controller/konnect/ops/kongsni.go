b44bb7fd7a7eb57ffb8e634dee3fa7645820470b
Grzegorz Burzyński <czeslavo@gmail.com>
2024-10-17 19:07:18 +0200

    fix: handle KongSNI conflict on creation

--- controller/konnect/ops/kongsni.go
+++ controller/konnect/ops/kongsni.go
@@ -11,4 +11,5 @@ type SNIsSDK interface {
 	CreateSniWithCertificate(context.Context, sdkkonnectops.CreateSniWithCertificateRequest, ...sdkkonnectops.Option) (*sdkkonnectops.CreateSniWithCertificateResponse, error)
 	UpsertSniWithCertificate(ctx context.Context, request sdkkonnectops.UpsertSniWithCertificateRequest, opts ...sdkkonnectops.Option) (*sdkkonnectops.UpsertSniWithCertificateResponse, error)
 	DeleteSniWithCertificate(ctx context.Context, request sdkkonnectops.DeleteSniWithCertificateRequest, opts ...sdkkonnectops.Option) (*sdkkonnectops.DeleteSniWithCertificateResponse, error)
+	ListSni(ctx context.Context, request sdkkonnectops.ListSniRequest, opts ...sdkkonnectops.Option) (*sdkkonnectops.ListSniResponse, error)
 }
