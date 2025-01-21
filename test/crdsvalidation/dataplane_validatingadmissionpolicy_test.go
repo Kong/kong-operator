0a2f693d73fa8f6addbe982565019b28a2ef000f
Patryk Małek <patryk.malek@konghq.com>
2025-01-21 19:10:18 +0100

    chore: fix ValidatingAdmissionPolicy for DataPlane ports

--- test/crdsvalidation/dataplane_validatingadmissionpolicy_test.go
+++ test/crdsvalidation/dataplane_validatingadmissionpolicy_test.go
@@ -35,7 +35,7 @@ func TestDataPlaneValidatingAdmissionPolicy(t *testing.T) {
 			Namespace:    ns.Name,
 		}
 		sharedEventuallyConfig = kcfgcrdsvalidation.EventuallyConfig{
-			Timeout: 15 * time.Second,
+			Timeout: 5 * time.Second,
 			Period:  100 * time.Millisecond,
 		}
 	)
@@ -255,6 +255,82 @@ func TestDataPlaneValidatingAdmissionPolicy(t *testing.T) {
 				},
 				ExpectedErrorEventuallyConfig: sharedEventuallyConfig,
 				ExpectedErrorMessage:          lo.ToPtr("is forbidden: ValidatingAdmissionPolicy 'ports.dataplane.gateway-operator.konghq.com' with binding 'binding-ports.dataplane.gateway-operator.konghq.com' denied request: Each port from spec.network.services.ingress.ports has to have an accompanying port in KONG_PORT_MAPS env"),
+			},
+			{
+				Name: "providing network services ingress options without ports does not fail",
+				TestObject: &operatorv1beta1.DataPlane{
+					ObjectMeta: commonObjectMeta,
+					Spec: operatorv1beta1.DataPlaneSpec{
+						DataPlaneOptions: operatorv1beta1.DataPlaneOptions{
+							Deployment: operatorv1beta1.DataPlaneDeploymentOptions{
+								DeploymentOptions: operatorv1beta1.DeploymentOptions{
+									PodTemplateSpec: &corev1.PodTemplateSpec{
+										Spec: corev1.PodSpec{
+											Containers: []corev1.Container{
+												{
+													Name:  "proxy",
+													Image: "kong:3.9",
+												},
+											},
+										},
+									},
+								},
+							},
+							Network: operatorv1beta1.DataPlaneNetworkOptions{
+								Services: &operatorv1beta1.DataPlaneServices{
+									Ingress: &operatorv1beta1.DataPlaneServiceOptions{
+										ServiceOptions: operatorv1beta1.ServiceOptions{
+											Annotations: map[string]string{
+												"a": "b",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			// TODO: ValidatingAdmissionPolicy rules have to be adjusted for this test case.
+			{
+				Name: "providing network services ingress ports without matching envs should fail",
+				TestObject: &operatorv1beta1.DataPlane{
+					ObjectMeta: commonObjectMeta,
+					Spec: operatorv1beta1.DataPlaneSpec{
+						DataPlaneOptions: operatorv1beta1.DataPlaneOptions{
+							Deployment: operatorv1beta1.DataPlaneDeploymentOptions{
+								DeploymentOptions: operatorv1beta1.DeploymentOptions{
+									PodTemplateSpec: &corev1.PodTemplateSpec{
+										Spec: corev1.PodSpec{
+											Containers: []corev1.Container{
+												{
+													Name:  "proxy",
+													Image: "kong:3.9",
+												},
+											},
+										},
+									},
+								},
+							},
+							Network: operatorv1beta1.DataPlaneNetworkOptions{
+								Services: &operatorv1beta1.DataPlaneServices{
+									Ingress: &operatorv1beta1.DataPlaneServiceOptions{
+										Ports: []operatorv1beta1.DataPlaneServicePort{
+											{
+												Name: "http",
+												Port: 80,
+												// No matching port in KONG_PORT_MAPS
+												TargetPort: intstr.FromInt(8001),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				ExpectedErrorMessage:          lo.ToPtr("Each port from spec.network.services.ingress.ports has to have an accompanying port in KONG_PORT_MAPS env"),
+				ExpectedErrorEventuallyConfig: sharedEventuallyConfig,
 			},
 		}.RunWithConfig(t, cfg, scheme)
 	})
