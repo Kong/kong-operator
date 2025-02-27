6b1bf34f5d48e2c0bf6a2f6b463b05594c91de13
Patryk Małek <patryk.malek@konghq.com>
2025-02-27 16:43:32 +0100

    chore: add a sample manifest for DataPlane with Konnect extension (#320)

--- api/konnect/v1alpha1/konnect_extension_types.go
+++ api/konnect/v1alpha1/konnect_extension_types.go
@@ -72,6 +72,7 @@ type KonnectExtensionSpec struct {
 	//
 	// +kubebuilder:validation:Required
 	KonnectControlPlane KonnectExtensionControlPlane `json:"konnectControlPlane"`
+
 	// DataPlaneClientAuth is the configuration for the client certificate authentication for the DataPlane.
 	// In case the ControlPlaneRef is of type KonnectID, it is required to set up the connection with the
 	// Konnect Platform.
