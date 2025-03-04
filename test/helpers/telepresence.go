da6c179d79f56019dd434ab4ceecc3419cff4b27
Jakub Warczarek <jakub.warczarek@konghq.com>
2025-03-04 14:09:29 +0100

    chore: manage telepresence installation and version with mise (#1274)

--- test/helpers/telepresence.go
+++ test/helpers/telepresence.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
 )
 
@@ -10,20 +11,29 @@ import (
 // It returns a cleanup function that should be called when the test is done.
 func SetupTelepresence(ctx context.Context) (func(), error) {
 	fmt.Println("INFO: installing telepresence traffic manager in the cluster")
-	out, err := exec.CommandContext(ctx, "telepresence", "helm", "install").CombinedOutput()
+	const telepresenceBin = "TELEPRESENCE_BIN"
+	telepresenceExecutable := os.Getenv(telepresenceBin)
+	if telepresenceExecutable == "" {
+		telepresenceExecutable = "telepresence"
+		fmt.Printf("WARN: environment variable %s is not set, try to fallback to a system wide 'telepresnce'", telepresenceBin)
+	} else {
+		fmt.Printf("INFO: path to binary from %s environment variable is %s\n", telepresenceBin, telepresenceExecutable)
+	}
+
+	out, err := exec.CommandContext(ctx, telepresenceExecutable, "helm", "install").CombinedOutput()
 	if err != nil {
 		return nil, fmt.Errorf("failed to install telepresence traffic manager: %w, %s", err, string(out))
 	}
 
 	fmt.Println("INFO: connecting to the cluster with telepresence")
-	out, err = exec.CommandContext(ctx, "telepresence", "connect").CombinedOutput()
+	out, err = exec.CommandContext(ctx, telepresenceExecutable, "connect").CombinedOutput()
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to the cluster with telepresence: %w, %s", err, string(out))
 	}
 
 	return func() {
 		fmt.Println("INFO: quitting telepresence daemons")
-		out, err := exec.CommandContext(ctx, "telepresence", "quit").CombinedOutput()
+		out, err := exec.CommandContext(ctx, telepresenceExecutable, "quit").CombinedOutput()
 		if err != nil {
 			fmt.Printf("ERROR: failed to quit telepresence daemons: %s\n", string(out))
 		}
