1033cfaceb13d12cd1d4bb28a18d404bd30f47b2
Yi Tao <tao.yi@konghq.com>
2025-03-07 10:59:33 +0800

    use WithKonnectControlPlaneRef

--- test/helpers/conditions.go
+++ test/helpers/conditions.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/samber/lo"
-
-	"github.com/kong/gateway-operator/pkg/consts"
 	k8sutils "github.com/kong/gateway-operator/pkg/utils/kubernetes"
+
+	kcfgconsts "github.com/kong/kubernetes-configuration/api/common/consts"
 )
 
 // CheckAllConditionsTrue returns true if all the conditions with given type in `conditionTypes` are set to `True` in the given resource.
 // If it returns `false`, the second return value contains a message to tell what conditions are not `True`.
-func CheckAllConditionsTrue(resource k8sutils.ConditionsAware, conditionTypes []consts.ConditionType) (bool, string) {
+func CheckAllConditionsTrue(resource k8sutils.ConditionsAware, conditionTypes []kcfgconsts.ConditionType) (bool, string) {
 	var failedConditions []string
-	lo.ForEach(conditionTypes, func(conditionType consts.ConditionType, _ int) {
+	for _, conditionType := range conditionTypes {
 		if !k8sutils.HasConditionTrue(conditionType, resource) {
 			failedConditions = append(failedConditions, string(conditionType))
 		}
-	})
+	}
+
 	if len(failedConditions) > 0 {
 		return false, fmt.Sprintf("condition(s) %s not set to True", strings.Join(failedConditions, ", "))
 	}
