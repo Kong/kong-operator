29823c62943207ad2523711b0615b966c034b03a
Jintao Zhang <zhangjintao9020@gmail.com>
2025-03-07 14:43:19 +0800

    chore: migrate KGO bluegreen conditions (#337)
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>
    Co-authored-by: Tao Yi <richardyi0110@hotmail.com>

--- api/gateway-operator/dataplane/conditions.go
+++ api/gateway-operator/dataplane/conditions.go
@@ -45,3 +45,59 @@ const (
 	// ResourceUpdatedMessage indicates a resource was updated
 	ResourceUpdatedMessage = "Resource has been updated"
 )
+
+// -----------------------------------------------------------------------------
+// DataPlane - BlueGreen Condition Constants
+// -----------------------------------------------------------------------------
+
+const (
+	// DataPlaneConditionTypeRolledOut is a condition type indicating whether or
+	// not, DataPlane's rollout has been successful or not.
+	DataPlaneConditionTypeRolledOut consts.ConditionType = "RolledOut"
+)
+
+const (
+	// DataPlaneConditionReasonRolloutAwaitingPromotion is a reason which indicates a DataPlane
+	// preview has been deployed successfully and is awaiting promotion.
+	// If this Reason is present and no automated rollout is disabled, user can
+	// use the preview services and deployment to inspect the state of those
+	// make a judgement call if the promotion should happen.
+	DataPlaneConditionReasonRolloutAwaitingPromotion consts.ConditionReason = "AwaitingPromotion"
+
+	// DataPlaneConditionReasonRolloutFailed is a reason which indicates a DataPlane
+	// has failed to roll out. This may be caused for example by a Deployment or
+	// a Service failing to get created during a rollout.
+	DataPlaneConditionReasonRolloutFailed consts.ConditionReason = "Failed"
+
+	// DataPlaneConditionReasonRolloutProgressing is a reason which indicates a DataPlane's
+	// new version is being rolled out.
+	DataPlaneConditionReasonRolloutProgressing consts.ConditionReason = "Progressing"
+
+	// DataPlaneConditionReasonRolloutWaitingForChange is a reason which indicates a DataPlane
+	// is waiting for a change to trigger new version to be made available before promotion.
+	DataPlaneConditionReasonRolloutWaitingForChange consts.ConditionReason = "WaitingForChange"
+
+	// DataPlaneConditionReasonRolloutPromotionInProgress is a reason which
+	// indicates that a promotion is in progress.
+	DataPlaneConditionReasonRolloutPromotionInProgress consts.ConditionReason = "PromotionInProgress"
+
+	// DataPlaneConditionReasonRolloutPromotionFailed is a reason which indicates
+	// a DataPlane has failed to promote. This may be caused for example by
+	// a failure in updating a live Service.
+	DataPlaneConditionReasonRolloutPromotionFailed consts.ConditionReason = "PromotionFailed"
+
+	// DataPlaneConditionReasonRolloutPromotionDone is a reason which indicates that a promotion is done.
+	DataPlaneConditionReasonRolloutPromotionDone consts.ConditionReason = "PromotionDone"
+)
+
+const (
+	// DataPlaneConditionMessageRolledOutRolloutInitialized contains the message
+	// that is set for the RolledOut Condition when Reason is Progressing
+	// and the DataPlane has initiated a rollout.
+	DataPlaneConditionMessageRolledOutRolloutInitialized = "Rollout initialized"
+
+	// DataPlaneConditionMessageRolledOutPreviewDeploymentNotYetReady contains the message
+	// that is set for the RolledOut Condition when Reason is Progressing
+	// and the operator is waiting for preview Deployment to be ready.
+	DataPlaneConditionMessageRolledOutPreviewDeploymentNotYetReady = "Preview Deployment not yet ready"
+)
