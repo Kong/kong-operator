7927ccdd4155683dea1a7cdcef348d1e46bc6450
Jintao Zhang <zhangjintao9020@gmail.com>
2025-04-29 13:27:28 +0000

    fix: fix the rollout restart op
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>
    Co-authored-by: Patryk Małek <patryk.malek@konghq.com>

--- test/integration/dataplane_scale_subresource_test.go
+++ test/integration/dataplane_scale_subresource_test.go
@@ -1,13 +1,17 @@
 package integration
 
 import (
+	"fmt"
+	"os/exec"
 	"testing"
+	"time"
 
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/kong/gateway-operator/pkg/consts"
@@ -16,6 +20,18 @@ import (
 
 	operatorv1beta1 "github.com/kong/kubernetes-configuration/api/gateway-operator/v1beta1"
 )
+
+// runKubectlScaleDataPlane runs the kubectl scale command to test the scale subresource for the DataPlane CRD.
+// It scales the DataPlane resource to the specified number of replicas and returns the output and error.
+func runKubectlScaleDataPlane(t *testing.T, namespacedName types.NamespacedName, replicas int) (string, error) {
+	t.Helper()
+
+	// Execute kubectl scale command
+	cmd := "kubectl scale --namespace=" + namespacedName.Namespace + " dataplane/" + namespacedName.Name + " --replicas=" + fmt.Sprint(replicas)
+
+	out, err := exec.CommandContext(t.Context(), "sh", "-c", cmd).CombinedOutput()
+	return string(out), err
+}
 
 // TestDataPlaneScaleSubresource tests the scale subresource of the DataPlane CRD.
 // It verifies that when a deployment is restarted using kubectl rollout restart,
@@ -38,35 +54,12 @@ func TestDataPlaneScaleSubresource(t *testing.T) {
 					DeploymentOptions: operatorv1beta1.DeploymentOptions{
 						Replicas: lo.ToPtr(int32(2)), // Set initial replica count to 2
 						PodTemplateSpec: &corev1.PodTemplateSpec{
-							ObjectMeta: metav1.ObjectMeta{
-								Labels: map[string]string{
-									"label-a": "value-a",
-									"label-x": "value-x",
-								},
-							},
 							Spec: corev1.PodSpec{
 								Containers: []corev1.Container{
 									{
-										Env: []corev1.EnvVar{
-											{
-												Name:  "TEST_ENV",
-												Value: "test",
-											},
-										},
 										Name:  consts.DataPlaneProxyContainerName,
 										Image: helpers.GetDefaultDataPlaneImage(),
 									},
-								},
-							},
-						},
-					},
-				},
-				Network: operatorv1beta1.DataPlaneNetworkOptions{
-					Services: &operatorv1beta1.DataPlaneServices{
-						Ingress: &operatorv1beta1.DataPlaneServiceOptions{
-							ServiceOptions: operatorv1beta1.ServiceOptions{
-								Annotations: map[string]string{
-									"purpose": "scale",
 								},
 							},
 						},
@@ -95,14 +88,96 @@ func TestDataPlaneScaleSubresource(t *testing.T) {
 		consts.GatewayOperatorManagedByLabel: consts.DataPlaneManagedLabelValue,
 	}, clients), testutils.GatewayReadyTimeLimit, testutils.ObjectUpdateTick)
 
-	// Test scaling through the scale subresource
-	t.Log("testing scaling through scale subresource")
-	require.Eventually(t,
-		testutils.DataPlaneUpdateEventually(t, GetCtx(), dataplaneName, clients, func(dp *operatorv1beta1.DataPlane) {
-			dp.Spec.Deployment.Replicas = lo.ToPtr(int32(3)) // Scale up to 3 replicas
-		}),
-		testutils.GatewayReadyTimeLimit, testutils.ObjectUpdateTick)
+	// Test scaling through the scale subresource - use kubectl scale
+	t.Log("testing scaling through scale subresource with kubectl scale")
+
+	// Using kubectl scale command to test the scale subresource
+	output, scaleErr := runKubectlScaleDataPlane(t, types.NamespacedName{
+		Namespace: namespace.Name,
+		Name:      dataplane.Name,
+	}, 3)
+	t.Logf("Scale command output: %s", output)
+	require.NoError(t, scaleErr)
 
 	t.Log("verifying dataplane scales to 3 replicas")
 	require.Eventually(t, testutils.DataPlaneHasNReadyPods(t, GetCtx(), dataplaneName, clients, 3), testutils.GatewayReadyTimeLimit, testutils.ObjectUpdateTick)
+
+	t.Log("simulating kubectl rollout restart by adding restart annotation")
+	deploymentCopy := deployment.DeepCopy()
+	if deploymentCopy.Spec.Template.Annotations == nil {
+		deploymentCopy.Spec.Template.Annotations = make(map[string]string)
+	}
+
+	restartTime := time.Now()
+	deploymentCopy.Spec.Template.Annotations["kubectl.kubernetes.io/restartedAt"] = restartTime.Format(time.RFC3339)
+
+	err = clients.MgrClient.Patch(GetCtx(), deploymentCopy, client.MergeFrom(&deployment))
+	require.NoError(t, err)
+
+	t.Log("waiting for new ReplicaSet to be created")
+
+	var newReplicaSet *appsv1.ReplicaSet
+	require.Eventually(t, func() bool {
+		rs := FindDataPlaneReplicaSetNewerThan(
+			t,
+			GetCtx(),
+			clients.MgrClient,
+			restartTime,
+			namespace.Name,
+			dataplane,
+		)
+		if rs != nil {
+			t.Logf("Found new ReplicaSet %s created at %v", rs.Name, rs.CreationTimestamp)
+			newReplicaSet = rs
+			return true
+		}
+		return false
+	}, testutils.GatewayReadyTimeLimit, testutils.ObjectUpdateTick, "Failed to find new ReplicaSet after rollout restart")
+
+	t.Log("verifying new ReplicaSet has correct replica count")
+	require.NotNil(t, newReplicaSet, "New ReplicaSet should be created")
+
+	// Wait for the ReplicaSet to have the correct replica count
+	require.Eventually(t, func() bool {
+		if err := clients.MgrClient.Get(GetCtx(), types.NamespacedName{
+			Namespace: newReplicaSet.Namespace,
+			Name:      newReplicaSet.Name,
+		}, newReplicaSet); err != nil {
+			t.Logf("Error getting ReplicaSet: %v", err)
+			return false
+		}
+		t.Logf("Current ReplicaSet replicas: %d", *newReplicaSet.Spec.Replicas)
+		return *newReplicaSet.Spec.Replicas == 3
+	}, testutils.GatewayReadyTimeLimit, testutils.ObjectUpdateTick, "New ReplicaSet should have 3 replicas")
+
+	t.Log("waiting for rollout to complete")
+	require.Eventually(t, func() bool {
+		if err := clients.MgrClient.Get(GetCtx(), types.NamespacedName{
+			Namespace: newReplicaSet.Namespace,
+			Name:      newReplicaSet.Name,
+		}, newReplicaSet); err != nil {
+			return false
+		}
+		return newReplicaSet.Status.ReadyReplicas == *newReplicaSet.Spec.Replicas
+	}, testutils.GatewayReadyTimeLimit, testutils.ObjectUpdateTick, "New ReplicaSet should have all replicas ready")
+
+	t.Log("verifying dataplane still has 3 ready replicas after rollout")
+	require.Eventually(t, testutils.DataPlaneHasNReadyPods(t, GetCtx(), dataplaneName, clients, 3), testutils.GatewayReadyTimeLimit, testutils.ObjectUpdateTick)
+
+	// Test the specific issue where scaling after restart didn't work properly
+	t.Log("scaling DataPlane to 4 replicas after restart")
+	// Test scaling through the scale subresource - use kubectl scale
+	t.Log("testing scaling through scale subresource with kubectl scale")
+
+	// Using kubectl scale command to test the scale subresource
+	output, scaleErr = runKubectlScaleDataPlane(t, types.NamespacedName{
+		Namespace: namespace.Name,
+		Name:      dataplane.Name,
+	}, 4)
+	t.Logf("Scale command output: %s", output)
+	require.NoError(t, scaleErr)
+
+	t.Log("verifying dataplane scales to 4 replicas")
+	require.Eventually(t, testutils.DataPlaneHasNReadyPods(t, GetCtx(), dataplaneName, clients, 4), testutils.GatewayReadyTimeLimit, testutils.ObjectUpdateTick)
+
 }
