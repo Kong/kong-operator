d691cfb8ee4698722d4ba66240c41695ba49a643
Jakub Warczarek <jakub.warczarek@konghq.com>
2025-05-29 14:33:40 +0200

    fix: data race for setting `cfssl` logger (#1690)
    * fix: remove data race for setting `cfssl` logger
    
    * from CR

--- controller/pkg/secrets/cert.go
+++ controller/pkg/secrets/cert.go
@@ -25,7 +25,6 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/kong/gateway-operator/controller/pkg/dataplane"
 	"github.com/kong/gateway-operator/controller/pkg/op"
@@ -37,6 +36,17 @@ import (
 
 	operatorv1beta1 "github.com/kong/kubernetes-configuration/api/gateway-operator/v1beta1"
 )
+
+var caLoggerInit sync.Once
+
+// SetCALogger sets the logger for the CFSSL signer. Call it once at the start
+// of the program to ensure that CFSSL logs are captured by the operator's logger.
+// Subsequent calls to this function will have no effect.
+func SetCALogger(logger logr.Logger) {
+	caLoggerInit.Do(func() {
+		cflog.SetLogger(loggerShim{logger: logger})
+	})
+}
 
 // -----------------------------------------------------------------------------
 // Private Functions - Certificate management
@@ -64,14 +74,6 @@ func (l loggerShim) Crit(msg string) { l.logger.V(logging.InfoLevel.Value()).Inf
 
 // Emerg logs on emergency level.
 func (l loggerShim) Emerg(msg string) { l.logger.V(logging.InfoLevel.Value()).Info(msg) }
-
-var caLoggerInit sync.Once
-
-func setCALogger(logger logr.Logger) {
-	caLoggerInit.Do(func() {
-		cflog.SetLogger(loggerShim{logger: logger})
-	})
-}
 
 /*
 Adapted from the Kubernetes CFSSL signer:
@@ -189,8 +191,6 @@ func EnsureCertificate[
 	cl client.Client,
 	additionalMatchingLabels client.MatchingLabels,
 ) (op.Result, *corev1.Secret, error) {
-	setCALogger(ctrllog.Log)
-
 	// Get the Secrets for the DataPlane using new labels.
 	matchingLabels := k8sresources.GetManagedLabelForOwner(owner)
 	for k, v := range additionalMatchingLabels {
