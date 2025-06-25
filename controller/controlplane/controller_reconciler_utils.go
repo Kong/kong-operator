c6c6563db7116300708094f46db663dcbbc52598
Patryk Małek <patryk.malek@konghq.com>
2025-06-25 14:24:42 +0200

    feat: reschedule ControlPlane manager when its spec changes (#1763)

--- controller/controlplane/controller_reconciler_utils.go
+++ controller/controlplane/controller_reconciler_utils.go
@@ -1,10 +1,18 @@
 package controlplane
 
 import (
+	"context"
 	"fmt"
 
+	certificatesv1 "k8s.io/api/certificates/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8stypes "k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/kong/gateway-operator/controller/pkg/op"
+	"github.com/kong/gateway-operator/controller/pkg/secrets"
+	"github.com/kong/gateway-operator/pkg/consts"
 	k8sutils "github.com/kong/gateway-operator/pkg/utils/kubernetes"
 
 	kcfgcontrolplane "github.com/kong/kubernetes-configuration/api/gateway-operator/controlplane"
@@ -74,4 +82,38 @@ func (r *Reconciler) ensureDataPlaneStatus(
 	default:
 		return false, fmt.Errorf("unsupported ControlPlane's DataPlane type: %s", cp.Spec.DataPlane.Type)
 	}
+}
+
+// ensureAdminMTLSCertificateSecret ensures that a Secret is created with the certificate for mTLS
+// communication between the ControlPlane and the DataPlane.
+func (r *Reconciler) ensureAdminMTLSCertificateSecret(
+	ctx context.Context,
+	cp *operatorv2alpha1.ControlPlane,
+) (
+	op.Result,
+	*corev1.Secret,
+	error,
+) {
+	usages := []certificatesv1.KeyUsage{
+		certificatesv1.UsageKeyEncipherment,
+		certificatesv1.UsageDigitalSignature,
+		certificatesv1.UsageClientAuth,
+	}
+	matchingLabels := client.MatchingLabels{
+		consts.SecretUsedByServiceLabel: consts.ControlPlaneServiceKindAdmin,
+	}
+	// this subject is arbitrary. data planes only care that client certificates are signed by the trusted CA, and will
+	// accept a certificate with any subject
+	return secrets.EnsureCertificate(ctx,
+		cp,
+		fmt.Sprintf("%s.%s", cp.Name, cp.Namespace),
+		k8stypes.NamespacedName{
+			Namespace: r.ClusterCASecretNamespace,
+			Name:      r.ClusterCASecretName,
+		},
+		usages,
+		r.ClusterCAKeyConfig,
+		r.Client,
+		matchingLabels,
+	)
 }
