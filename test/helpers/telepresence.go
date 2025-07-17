cc11e03ee85d79735561de414136c3558f4ff596
Aldo Lacuku <aldo@lacuku.eu>
2025-07-17 17:08:34 +0200

    fix(test/helpers): handle telepresence traffic manager upgrade if already installed
    Update SetupTelepresence to run `telepresence helm upgrade` if `telepresence helm install` fails with
    a message indicating the manager is already installed. This improves idempotency and error handling,
    making it handy when running tests multiple times and reusing the same environment.
    
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- test/helpers/telepresence.go
+++ test/helpers/telepresence.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/kong/kong-operator/test"
 )
@@ -28,7 +29,11 @@ func SetupTelepresence(ctx context.Context) (func(), error) {
 	}
 
 	out, err := exec.CommandContext(ctx, telepresenceExecutable, "helm", "install").CombinedOutput()
-	if err != nil {
+	if err != nil && strings.Contains(string(out), "use 'telepresence helm upgrade' instead to replace it") {
+		if out, err := exec.CommandContext(ctx, telepresenceExecutable, "helm", "upgrade").CombinedOutput(); err != nil {
+			return nil, fmt.Errorf("failed to upgrade telepresence traffic manager: %w, %s", err, string(out))
+		}
+	} else if err != nil {
 		return nil, fmt.Errorf("failed to install telepresence traffic manager: %w, %s", err, string(out))
 	}
 
