ac9b500fad59cff73c2f747c96ee12d0b9946458
Patryk Małek <patryk.malek@konghq.com>
2025-09-22 16:42:37 +0200

    tests: fix TestSetupControllers

--- modules/manager/controller_setup_test.go
+++ modules/manager/controller_setup_test.go
@@ -23,7 +23,7 @@ func TestSetupControllers(t *testing.T) {
 	controllerDefs, err := manager.SetupControllers(mgr, &cfg, nil)
 	require.NoError(t, err)
 
-	const expectedControllerCount = 44
+	const expectedControllerCount = 42
 	require.Len(t, controllerDefs, expectedControllerCount)
 
 	seenControllerTypes := make(map[string]int, expectedControllerCount)
