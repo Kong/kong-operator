ee733a8c7fe8913ee3ac8c9bf53dd3c73c3f0640
Aldo Lacuku <aldo@lacuku.eu>
2025-09-26 15:10:39 +0200

    chore: apply review suggestions
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- controller/hybridgateway/reconciler_utils.go
+++ controller/hybridgateway/reconciler_utils.go
@@ -48,47 +48,47 @@ func EnforceState[t converter.RootObject](ctx context.Context, cl client.Client,
 			Name:      desired.GetName(),
 		}, existing)
 
+		namespacedNameDesired := client.ObjectKeyFromObject(&desired)
+		namespacedNameExisting := client.ObjectKeyFromObject(existing)
+
 		if err != nil {
 			if errors.IsNotFound(err) {
 				// Object doesn't exist, create it using server-side apply.
-				logger.V(1).Info("Creating new object", "kind", desired.GetKind(), "name", desired.GetName(), "namespace", desired.GetNamespace())
+				logger.V(1).Info("Creating new object", "kind", desired.GetKind(), "obj", namespacedNameDesired)
 
 				// Set field manager for server-side apply
 				if err := cl.Patch(ctx, &desired, client.Apply, client.FieldOwner(FieldManager), client.ForceOwnership); err != nil {
 					if errors.IsConflict(err) {
-						logger.V(1).Info("Conflict during create, will retry", "error", err)
-						return true, false, nil
+						return true, false, fmt.Errorf("conflict during create of object kind %s obj %s: %w", desired.GetKind(), namespacedNameDesired, err)
 					}
-					return true, false, fmt.Errorf("failed to create object kind %s name %s: %w", desired.GetKind(), desired.GetName(), err)
+					return true, false, fmt.Errorf("failed to create object kind %s obj %s: %w", desired.GetKind(), namespacedNameDesired, err)
 				}
 				continue
 			} else {
 				// Other error getting the object.
-				logger.Error(err, "Failed to get existing object", "kind", desired.GetKind(), "name", desired.GetName())
-				return true, false, err
+				return true, false, fmt.Errorf("failed to get object kind %s obj %s: %w", desired.GetKind(), namespacedNameDesired, err)
 			}
 		}
 
 		// Handle the case when resource are marked for deletion.
 		if !existing.GetDeletionTimestamp().IsZero() {
-			logger.V(1).Info("Existing object is marked for deletion, will not enforce state", "kind", existing.GetKind(), "name", existing.GetName())
+			logger.V(1).Info("Existing object is marked for deletion, will not enforce state", "kind", existing.GetKind(), "obj", namespacedNameDesired)
 			continue
 		}
 
 		// Object exists, check if we need to update it.
 		managedFieldsObj, err := managedfields.ExtractAsUnstructured(existing, FieldManager, "")
 		if err != nil {
-			return true, false, fmt.Errorf("failed to extract managed fields for kind %s name %s: %w", existing.GetKind(), existing.GetName(), err)
+			return true, false, fmt.Errorf("failed to extract managed fields for kind %s obj %s: %w", existing.GetKind(), namespacedNameExisting, err)
 		}
 		if managedFieldsObj == nil {
 			// No managed fields for our field manager, we should update.
-			logger.V(1).Info("No managed fields found for our field manager, will apply desired state", "kind", existing.GetKind(), "name", existing.GetName())
+			logger.V(1).Info("No managed fields found for our field manager, will apply desired state", "kind", existing.GetKind(), "obj", namespacedNameExisting)
 			if err := cl.Patch(ctx, &desired, client.Apply, client.FieldOwner(FieldManager), client.ForceOwnership); err != nil {
 				if errors.IsConflict(err) {
-					logger.V(1).Info("Conflict during create, will retry", "error", err)
-					return true, false, nil
+					return true, false, fmt.Errorf("conflict during create of object kind %s obj %s: %w", desired.GetKind(), namespacedNameDesired, err)
 				}
-				return true, false, fmt.Errorf("failed to create object kind %s name %s: %w", desired.GetKind(), desired.GetName(), err)
+				return true, false, fmt.Errorf("failed to create object kind %s obj %s: %w", desired.GetKind(), namespacedNameDesired, err)
 			}
 			continue
 		}
@@ -96,26 +96,25 @@ func EnforceState[t converter.RootObject](ctx context.Context, cl client.Client,
 		// Convert desired resource to unstructured.
 		desiredU, err := utils.ToUnstructured(&desired, cl.Scheme())
 		if err != nil {
-			return true, false, fmt.Errorf("failed to convert to unstructured desired resources for kind %s name %s: %w", desired.GetKind(), desired.GetName(), err)
+			return true, false, fmt.Errorf("failed to convert to unstructured desired obj for kind %s obj %s: %w", desired.GetKind(), namespacedNameDesired, err)
 		}
 
 		// Compare the two states.
 		compare, err := managedfields.Compare(managedFieldsObj, pruneDesiredObj(desiredU))
 		if err != nil {
-			return true, false, fmt.Errorf("failed to compare managed fields for kind %s name %s: %w", existing.GetKind(), existing.GetName(), err)
+			return true, false, fmt.Errorf("failed to compare managed fields for kind %s obj %s: %w", existing.GetKind(), namespacedNameExisting, err)
 		}
 
 		if compare.IsSame() {
-			logger.V(3).Info("No changes detected for resource", "kind", existing.GetKind(), "name", existing.GetName())
+			logger.V(3).Info("No changes detected for obj", "kind", existing.GetKind(), "obj", namespacedNameExisting)
 		} else {
-			logger.Info("Changes detected for resource, applying desired state", "kind", existing.GetKind(), "name", existing.GetName(), "changes", compare.String())
+			logger.Info("Changes detected for obj, applying desired state", "kind", existing.GetKind(), "obj", namespacedNameExisting, "changes", compare.String())
 			// Changes detected, apply the desired state using server-side apply.
 			if err := cl.Patch(ctx, &desired, client.Apply, client.FieldOwner(FieldManager), client.ForceOwnership); err != nil {
 				if errors.IsConflict(err) {
-					logger.V(1).Info("Conflict during update, will retry", "error", err)
-					return true, false, nil
+					return true, false, fmt.Errorf("conflict during create of object kind %s obj %s: %w", desired.GetKind(), namespacedNameDesired, err)
 				}
-				return true, false, fmt.Errorf("failed to update object kind %s name %s: %w", desired.GetKind(), desired.GetName(), err)
+				return true, false, fmt.Errorf("failed to update object kind %s obj %s: %w", desired.GetKind(), namespacedNameDesired, err)
 			}
 		}
 	}
@@ -155,16 +154,16 @@ func CleanOrphanedResources[t converter.RootObject, tPtr converter.RootObjectPtr
 		ns := rootObjPtr.GetNamespace()
 
 		if err := cl.List(ctx, list, selector, client.InNamespace(ns)); err != nil {
-			return fmt.Errorf("unable to list objects with gvk %s: %w", gvk.String(), err)
+			return fmt.Errorf("unable to list objects with gvk %s in namespace %s: %w", gvk.String(), ns, err)
 		}
 
 		for _, item := range list.Items {
 			key := fmt.Sprintf("%s/%s/%s", item.GetNamespace(), item.GetName(), gvk.String())
 			if _, found := desiredSet[key]; !found {
 				// Not in desired output, delete it.
-				logger.Info("Deleting orphaned resource", "kind", item.GetKind(), "name", item.GetName(), "namespace", item.GetNamespace())
+				logger.Info("Deleting orphaned resource", "kind", item.GetKind(), "obj", client.ObjectKeyFromObject(&item))
 				if err := cl.Delete(ctx, &item); err != nil && !errors.IsNotFound(err) {
-					return fmt.Errorf("failed to delete orphaned resource kind %s name %s: %w", item.GetKind(), item.GetName(), err)
+					return fmt.Errorf("failed to delete orphaned resource kind %s obj %s: %w", item.GetKind(), client.ObjectKeyFromObject(&item), err)
 				}
 			}
 		}
@@ -175,6 +174,7 @@ func CleanOrphanedResources[t converter.RootObject, tPtr converter.RootObjectPtr
 // pruneDesiredObj removes fields that should not be compared when checking for differences.
 func pruneDesiredObj(obj unstructured.Unstructured) *unstructured.Unstructured {
 	u := obj.DeepCopy()
+	// Remove metadata fields such as name and namespace from the desired object that are not managed by the controller.
 	unstructured.RemoveNestedField(u.Object, "metadata", "name")
 	unstructured.RemoveNestedField(u.Object, "metadata", "namespace")
 	managedfields.PruneEmptyFields(u)
