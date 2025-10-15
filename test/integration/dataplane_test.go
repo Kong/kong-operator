3cfbaf9cfe017141b6d89c4907ae6c6e5c084454
Sandro Modarelli <sandro.modarelli@gmail.com>
2025-10-15 13:36:13 +0200

    fix(test): make lint happy

--- test/integration/dataplane_test.go
+++ test/integration/dataplane_test.go
@@ -26,6 +26,7 @@ import (
 	operatorv1beta1 "github.com/kong/kubernetes-configuration/v2/api/gateway-operator/v1beta1"
 	konnectv1alpha1 "github.com/kong/kubernetes-configuration/v2/api/konnect/v1alpha1"
 	konnectv1alpha2 "github.com/kong/kubernetes-configuration/v2/api/konnect/v1alpha2"
+
 	"github.com/kong/kong-operator/controller/dataplane/certificates"
 	"github.com/kong/kong-operator/pkg/consts"
 	k8sutils "github.com/kong/kong-operator/pkg/utils/kubernetes"
