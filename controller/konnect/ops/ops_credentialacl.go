09797653301ecc2dd45b55615b2c356cd4e9f03d
Jintao Zhang <zhangjintao9020@gmail.com>
2025-10-24 18:15:52 +0000

    feat(konnect): Implement adoption for Credentials
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- controller/konnect/ops/ops_credentialacl.go
+++ controller/konnect/ops/ops_credentialacl.go
@@ -2,12 +2,15 @@ package ops
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	sdkkonnectcomp "github.com/Kong/sdk-konnect-go/models/components"
 	sdkkonnectops "github.com/Kong/sdk-konnect-go/models/operations"
 	"github.com/samber/lo"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	commonv1alpha1 "github.com/kong/kong-operator/api/common/v1alpha1"
 	configurationv1alpha1 "github.com/kong/kong-operator/api/configuration/v1alpha1"
 	sdkops "github.com/kong/kong-operator/controller/konnect/ops/sdk"
 )
@@ -101,6 +104,74 @@ func deleteKongCredentialACL(
 	return nil
 }
 
+func adoptKongCredentialACL(
+	ctx context.Context,
+	sdk sdkops.KongCredentialACLSDK,
+	cred *configurationv1alpha1.KongCredentialACL,
+) error {
+	cpID := cred.GetControlPlaneID()
+	if cpID == "" {
+		return errors.New("No Control Plane ID")
+	}
+	if cred.Status.Konnect == nil || cred.Status.Konnect.GetConsumerID() == "" {
+		return fmt.Errorf("can't adopt %T %s without a Konnect Consumer ID", cred, client.ObjectKeyFromObject(cred))
+	}
+	if cred.Spec.Adopt == nil || cred.Spec.Adopt.Konnect == nil {
+		return fmt.Errorf("missing Konnect adoption options for %T %s", cred, client.ObjectKeyFromObject(cred))
+	}
+
+	adoptOptions := cred.Spec.Adopt
+	konnectID := adoptOptions.Konnect.ID
+
+	resp, err := sdk.GetACLWithConsumer(ctx, sdkkonnectops.GetACLWithConsumerRequest{
+		ControlPlaneID:              cpID,
+		ConsumerIDForNestedEntities: cred.Status.Konnect.GetConsumerID(),
+		ACLID:                       konnectID,
+	})
+	if err != nil {
+		return KonnectEntityAdoptionFetchError{
+			KonnectID: konnectID,
+			Err:       err,
+		}
+	}
+	if resp == nil || resp.ACL == nil {
+		return fmt.Errorf("failed to adopt %s: %w", cred.GetTypeName(), ErrNilResponse)
+	}
+
+	uidTag, hasUIDTag := findUIDTag(resp.ACL.Tags)
+	if hasUIDTag && extractUIDFromTag(uidTag) != string(cred.UID) {
+		return KonnectEntityAdoptionUIDTagConflictError{
+			KonnectID:    konnectID,
+			ActualUIDTag: extractUIDFromTag(uidTag),
+		}
+	}
+
+	adoptMode := adoptOptions.Mode
+	if adoptMode == "" {
+		adoptMode = commonv1alpha1.AdoptModeOverride
+	}
+
+	switch adoptMode {
+	case commonv1alpha1.AdoptModeOverride:
+		credCopy := cred.DeepCopy()
+		credCopy.SetKonnectID(konnectID)
+		if err = updateKongCredentialACL(ctx, sdk, credCopy); err != nil {
+			return err
+		}
+	case commonv1alpha1.AdoptModeMatch:
+		if !credentialACLMatch(resp.ACL, cred) {
+			return KonnectEntityAdoptionNotMatchError{
+				KonnectID: konnectID,
+			}
+		}
+	default:
+		return fmt.Errorf("failed to adopt: adopt mode %q not supported", adoptMode)
+	}
+
+	cred.SetKonnectID(konnectID)
+	return nil
+}
+
 func kongCredentialACLToACLWithoutParents(
 	cred *configurationv1alpha1.KongCredentialACL,
 ) sdkkonnectcomp.ACLWithoutParents {
@@ -136,4 +207,15 @@ func getKongCredentialACLForUID(
 	x := sliceToEntityWithIDPtrSlice(resp.Object.Data)
 
 	return getMatchingEntryFromListResponseData(x, cred)
+}
+
+func credentialACLMatch(
+	konnectACL *sdkkonnectcomp.ACL,
+	cred *configurationv1alpha1.KongCredentialACL,
+) bool {
+	if konnectACL == nil {
+		return false
+	}
+
+	return konnectACL.Group == cred.Spec.Group
 }
--- test/mocks/sdkmocks/zz_generated.credentialapikey.go
+++ test/mocks/sdkmocks/zz_generated.credentialapikey.go
@@ -369,3 +369,86 @@ func (_c *MockKongCredentialAPIKeySDK_UpsertKeyAuthWithConsumer_Call) RunAndRetu
 	_c.Call.Return(run)
 	return _c
 }
+
+// GetKeyAuthWithConsumer provides a mock function for the type MockKongCredentialAPIKeySDK
+func (_mock *MockKongCredentialAPIKeySDK) GetKeyAuthWithConsumer(ctx context.Context, request operations.GetKeyAuthWithConsumerRequest, opts ...operations.Option) (*operations.GetKeyAuthWithConsumerResponse, error) {
+	var tmpRet mock.Arguments
+	if len(opts) > 0 {
+		tmpRet = _mock.Called(ctx, request, opts)
+	} else {
+		tmpRet = _mock.Called(ctx, request)
+	}
+	ret := tmpRet
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetKeyAuthWithConsumer")
+	}
+
+	var r0 *operations.GetKeyAuthWithConsumerResponse
+	var r1 error
+	if returnFunc, ok := ret.Get(0).(func(context.Context, operations.GetKeyAuthWithConsumerRequest, ...operations.Option) (*operations.GetKeyAuthWithConsumerResponse, error)); ok {
+		return returnFunc(ctx, request, opts...)
+	}
+	if returnFunc, ok := ret.Get(0).(func(context.Context, operations.GetKeyAuthWithConsumerRequest, ...operations.Option) *operations.GetKeyAuthWithConsumerResponse); ok {
+		r0 = returnFunc(ctx, request, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*operations.GetKeyAuthWithConsumerResponse)
+		}
+	}
+	if returnFunc, ok := ret.Get(1).(func(context.Context, operations.GetKeyAuthWithConsumerRequest, ...operations.Option) error); ok {
+		r1 = returnFunc(ctx, request, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+	return r0, r1
+}
+
+// MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetKeyAuthWithConsumer'
+type MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call struct {
+	*mock.Call
+}
+
+// GetKeyAuthWithConsumer is a helper method to define mock.On call
+//   - ctx context.Context
+//   - request operations.GetKeyAuthWithConsumerRequest
+//   - opts ...operations.Option
+func (_e *MockKongCredentialAPIKeySDK_Expecter) GetKeyAuthWithConsumer(ctx interface{}, request interface{}, opts ...interface{}) *MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call {
+	return &MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call{Call: _e.mock.On("GetKeyAuthWithConsumer",
+		append([]interface{}{ctx, request}, opts...)...)}
+}
+
+func (_c *MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call) Run(run func(ctx context.Context, request operations.GetKeyAuthWithConsumerRequest, opts ...operations.Option)) *MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		var arg1 operations.GetKeyAuthWithConsumerRequest
+		if args[1] != nil {
+			arg1 = args[1].(operations.GetKeyAuthWithConsumerRequest)
+		}
+		var arg2 []operations.Option
+		var variadicArgs []operations.Option
+		if len(args) > 2 {
+			variadicArgs = args[2].([]operations.Option)
+		}
+		arg2 = variadicArgs
+		run(
+			arg0,
+			arg1,
+			arg2...,
+		)
+	})
+	return _c
+}
+
+func (_c *MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call) Return(getKeyAuthWithConsumerResponse *operations.GetKeyAuthWithConsumerResponse, err error) *MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call {
+	_c.Call.Return(getKeyAuthWithConsumerResponse, err)
+	return _c
+}
+
+func (_c *MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call) RunAndReturn(run func(ctx context.Context, request operations.GetKeyAuthWithConsumerRequest, opts ...operations.Option) (*operations.GetKeyAuthWithConsumerResponse, error)) *MockKongCredentialAPIKeySDK_GetKeyAuthWithConsumer_Call {
+	_c.Call.Return(run)
+	return _c
+}
