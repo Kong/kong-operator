dff110891da1f093872ff67d6547ad5f271daeb5
Francesco Giudici <francesco.giudici@konghq.com>
2025-11-19 10:57:08 +0100

    Add clean-up on HTTPRoute deletion
    Signed-off-by: Francesco Giudici <francesco.giudici@konghq.com>

--- controller/hybridgateway/const/route/common.go
+++ controller/hybridgateway/const/route/common.go
@@ -2,3 +2,6 @@ package route
 
 // HTTPRouteKey identifies HTTPRoute resources.
 const HTTPRouteKey = "HTTPRoute"
+
+// RouteFinalizer is the finalizer added to Route objects to manage cleanup of generated resources.
+const RouteFinalizer = "gateway-operator.konghq.com/route-cleanup"
