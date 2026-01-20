9148d5ab9af8c5d7a6d94e9fe602f3dc70d2aea9
Jintao Zhang <zhangjintao9020@gmail.com>
2026-01-20 17:56:50 +0800

    fix: set Programmed condition to False when KongCertificateRef is not valid
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- controller/konnect/reconciler_generic.go
+++ controller/konnect/reconciler_generic.go
@@ -331,6 +331,13 @@ func (r *KonnectEntityReconciler[T, TEnt]) Reconcile(
 
 		return patchWithProgrammedStatusConditionBasedOnOtherConditions(ctx, r.Client, ent)
 	} else if !res.IsZero() {
+		// If the result is not zero (e.g., requeue), we still need to update the Programmed
+		// status condition based on other conditions (e.g., KongCertificateRefValid set to False
+		// when referenced KongCertificate is not programmed yet).
+		// We patch the status but still return the original requeue result.
+		if _, errStatus := patchWithProgrammedStatusConditionBasedOnOtherConditions(ctx, r.Client, ent); errStatus != nil {
+			return ctrl.Result{}, errStatus
+		}
 		return res, nil
 	}
 
