cd3de07d79bf6efa9d3ec153a4099ecca6d0d1f9
Yi Tao <tao.yi@konghq.com>
2026-01-20 17:05:16 +0800

    Add gateway count provider in telemetry

---
+++ internal/telemetry/provider_gatewaycount.go
@@ -0,0 +1,162 @@
+package telemetry
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	telemetryprovider "github.com/kong/kubernetes-telemetry/pkg/provider"
+	telemetrytypes "github.com/kong/kubernetes-telemetry/pkg/types"
+	"github.com/samber/lo"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
+
+	operatorv2beta1 "github.com/kong/kong-operator/api/gateway-operator/v2beta1"
+	"github.com/kong/kong-operator/internal/utils/gatewayclass"
+)
+
+const (
+	// GatewayCountProviderName is the name of the gateway count provider.
+	GatewayCountProviderName = "gateways"
+	// GatewayCountProviderKind is the kind of the gateway count provider.
+	GatewayCountProviderKind = telemetryprovider.Kind("gateway_count")
+
+	// ReconciledGatewayCountKey is the key for count of gateways reconciled by the controller.
+	ReconciledGatewayCountKey = "k8s_gateways_reconciled_count"
+	// ProgrammedGatewayCountKey is the key for count of gateways successfully configured ("Programmed") by the controller.
+	ProgrammedGatewayCountKey = "k8s_gateway_programmed_count"
+	// HybridGatewayCountKey is the key for count of Konnect hybrid gateways.
+	HybridGatewayCountKey = "konnect_hybrid_gateway_count"
+	// ProgrammedHybridGatewayCountKey is the key for count of programmed Konnect hybrid gateways.
+	ProgrammedHybridGatewayCountKey = "konnect_hybrid_gateway_programmed_count"
+
+	defaultPageSize = 1000
+)
+
+// gatewayCountProvider is the provider to provide telemetry data of count of reconciled and programmed Gateways.
+type gatewayCountProvider struct {
+	konnectEnabled bool
+
+	cl client.Client
+}
+
+var _ telemetryprovider.Provider = &gatewayCountProvider{}
+
+// Name returns the name of the Gateway count provider.
+func (p *gatewayCountProvider) Name() string {
+	return GatewayCountProviderName
+}
+
+// Kind returns the kind of the Gateway count provider.
+func (p *gatewayCountProvider) Kind() telemetryprovider.Kind {
+	return GatewayCountProviderKind
+}
+
+// Provide provides the telemetry data when anonymous reports are enabled on the reconciler, including:
+// - Number of reconciled Gateways
+// - Number of programmed Gateways
+// - Number of reconciled Konnect hybrid gateways
+// - Number of programmed Konnect hybrid gateways
+func (p *gatewayCountProvider) Provide(ctx context.Context) (telemetrytypes.ProviderReport, error) {
+
+	var (
+		reconciledGatewayCount       int
+		programmedGatewayCount       int
+		hybridGatewayCount           int
+		programmedHybridGatewayCount int
+
+		continueToken string
+	)
+
+	// List all gateways and count the number of each type required for telemetry data.
+	for {
+		gatewayList := gatewayv1.GatewayList{}
+		err := p.cl.List(ctx, &gatewayList, &client.ListOptions{
+			Limit:    defaultPageSize,
+			Continue: continueToken,
+		})
+
+		if err != nil {
+			return nil, err
+		}
+		// Check each Gateway and increase the count if the Gateway satisfies the conditions of counting.
+		for _, gw := range gatewayList.Items {
+			// Check if the gateway is reconciled and continue checking only when it is.
+			gwc, err := gatewayclass.Get(ctx, p.cl, string(gw.Spec.GatewayClassName))
+			if err != nil {
+				continue
+			}
+			reconciledGatewayCount++
+			// Check if the Gateway is programmed.
+			programmed := lo.ContainsBy(gw.Status.Conditions, func(condition metav1.Condition) bool {
+				return condition.Type == "Programmed" && condition.Status == metav1.ConditionTrue
+			})
+			if programmed {
+				programmedGatewayCount++
+				// TODO: count the number of attached routes on each "programmed" listener.
+			}
+			// Check if the gateway is Konnect hybrid gateway only when Konnect controller is enabled.
+			if p.konnectEnabled {
+				gwConfig, err := p.getGatewayConfigForGatewayClass(ctx, gwc.GatewayClass)
+				if err != nil {
+					continue
+				}
+				// Count the hybrid gateway if the GatewayConfiguration specifies a Konnect API auth config.
+				if gwConfig.Spec.Konnect != nil && gwConfig.Spec.Konnect.APIAuthConfigurationRef != nil {
+					hybridGatewayCount++
+					if programmed {
+						programmedHybridGatewayCount++
+					}
+				}
+			}
+		}
+
+		if continueToken = gatewayList.GetContinue(); continueToken == "" {
+			break
+		}
+	}
+	// Collect and return the report.
+	report := telemetrytypes.ProviderReport{
+		ReconciledGatewayCountKey: reconciledGatewayCount,
+		ProgrammedGatewayCountKey: programmedGatewayCount,
+	}
+	if p.konnectEnabled {
+		report[HybridGatewayCountKey] = hybridGatewayCount
+		report[ProgrammedHybridGatewayCountKey] = programmedHybridGatewayCount
+	}
+	return report, nil
+}
+
+// getGatewayConfigForGatewayClass gets the GatewayConfiguration specified for the given GatewayClass.
+// If the spec.parametersRef is empty, it returns a default GatewayConfiguration.
+// If the kind in spec.parametersRef does not match, or namespace/name is not specified, it returns error.
+// TODO: DRY -- extract the getGatewayConfigForParametersRef in gateway reconciler elsewhere and reuse it.
+func (p *gatewayCountProvider) getGatewayConfigForGatewayClass(
+	ctx context.Context, gwc *gatewayv1.GatewayClass,
+) (operatorv2beta1.GatewayConfiguration, error) {
+	if gwc.Spec.ParametersRef == nil {
+		return operatorv2beta1.GatewayConfiguration{}, nil
+	}
+	parametersRef := gwc.Spec.ParametersRef
+	if parametersRef.Group != gatewayv1.Group(operatorv2beta1.SchemeGroupVersion.Group) ||
+		parametersRef.Kind != "GatewayConfiguration" {
+		return operatorv2beta1.GatewayConfiguration{},
+			fmt.Errorf("controller only supports %s %s resources for GatewayClass parametersRef",
+				operatorv2beta1.SchemeGroupVersion.Group, "GatewayConfiguration")
+	}
+	if parametersRef.Namespace == nil {
+		return operatorv2beta1.GatewayConfiguration{}, errors.New("namespace must be specified")
+	}
+	if parametersRef.Name == "" {
+		return operatorv2beta1.GatewayConfiguration{}, errors.New("name must be specified")
+	}
+
+	nn := client.ObjectKey{
+		Namespace: string(*parametersRef.Namespace),
+		Name:      parametersRef.Name,
+	}
+	gwConfig := operatorv2beta1.GatewayConfiguration{}
+	err := p.cl.Get(ctx, nn, &gwConfig)
+	return gwConfig, err
+}
