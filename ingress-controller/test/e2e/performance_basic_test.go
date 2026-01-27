405a6565741eb6fb63fc65b714e95b85e7d7ed76
Ubuntu <moelove@lecloud.pqiwmov4ff1u3nbmqg3i55n5ob.lx.internal.cloudapp.net>
2026-01-27 07:10:55 +0000

    move ingress-controller integration to test/integration/kic
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- ingress-controller/test/e2e/performance_basic_test.go
+++ ingress-controller/test/e2e/performance_basic_test.go
@@ -23,7 +23,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/kong/kong-operator/ingress-controller/test"
-	"github.com/kong/kong-operator/ingress-controller/test/internal/helpers"
+	"github.com/kong/kong-operator/ingress-controller/test/helpers"
 )
 
 var (
