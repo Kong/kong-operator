9f0b5638a5e66e1bd35f8622150b78303483f51b
Patryk Małek <patryk.malek@konghq.com>
2026-02-10 19:16:55 +0100

    wip

--- crd-from-oas/pkg/generator/templates.go
+++ crd-from-oas/pkg/generator/templates.go
@@ -18,6 +18,9 @@ import (
 // +kubebuilder:printcolumn:name="Programmed",description="The Resource is Programmed on Konnect",type=string,JSONPath=` + "`" + `.status.conditions[?(@.type=='Programmed')].status` + "`" + `
 // +kubebuilder:printcolumn:name="OrgID",description="Konnect Organization ID this resource belongs to.",type=string,JSONPath=` + "`" + `.status.organizationID` + "`" + `
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:storageversion
+// +apireference:kgo:include
+// +kong:channels=kong-operator
 type {{.EntityName}} struct {
 	metav1.TypeMeta   ` + "`" + `json:",inline"` + "`" + `
 	metav1.ObjectMeta ` + "`" + `json:"metadata,omitzero"` + "`" + `
