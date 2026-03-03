1fedecdaab6c1496498164bda610599cfb4ae3f9
Patryk Małek <patryk.malek@konghq.com>
2026-03-03 10:05:11 +0100

    chore: use kind and ktf managed by mise

--- scripts/charts-test-env.sh
+++ scripts/charts-test-env.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # ------------------------------------------------------------------------------
-#
 # This script is used predominantly by CI in order to deploy a testing environment
 # for running the chart tests in this repository. The testing environment includes
 # a fully functional Kubernetes cluster, usually based on a local Kubernetes
@@ -11,12 +10,30 @@
 #       created here can be torn down with `ktf environments delete --name <NAME>`.
 # ------------------------------------------------------------------------------
 
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "${SCRIPT_DIR}/.."
+
+[[ -z ${KUBERNETES_VERSION} ]] && echo "ERROR: KUBERNETES_VERSION is not set" && exit 1
+
+# ------------------------------------------------------------------------------
+# Setup Tools - KTF
+# ------------------------------------------------------------------------------
+
+# ensure ktf command is accessible
+if ! command -v ktf 1>/dev/null
+then
+	echo "ERROR: ktf command not found"
+    exit 1
+fi
+
 # ------------------------------------------------------------------------------
 # Configuration
 # ------------------------------------------------------------------------------
 
 TEST_ENV_NAME="${TEST_ENV_NAME:-kong-charts-tests}"
-if [[ -n $1 ]]
+if [[ "$#" -eq 1 ]]
 then
     if [[ "$1" == "cleanup" ]]
     then
@@ -24,17 +41,6 @@ then
         exit $?
     fi
 fi
-
-set -euo pipefail
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "${SCRIPT_DIR}/.."
-CHART_NAME="${CHART_NAME:-ingress}"
-OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
-ARCH="$(uname -m | sed 's/x86_64/amd64/' | sed 's/aarch64/arm64/')"
-KTF_URL=https://github.com/Kong/kubernetes-testing-framework/releases/latest/download/ktf.${OS}.${ARCH}
-
-[[ -z ${KUBERNETES_VERSION} ]] && echo "ERROR: KUBERNETES_VERSION is not set" && exit 1
 
 # ------------------------------------------------------------------------------
 # Setup Tools - Docker
@@ -57,31 +63,12 @@ docker info 1>/dev/null
 # ensure kind command is accessible
 if ! command -v kind &> /dev/null
 then
-    [[ -z ${KIND_VERSION} ]] && echo "ERROR: KIND_VERSION is not set" && exit 1
-    echo "Installing sigs.k8s.io/kind, version ${KIND_VERSION}"
-    go install sigs.k8s.io/kind@v"${KIND_VERSION}"
+    echo "ERROR: kind command not found"
+    exit 1
 fi
 
 # ensure kind is functional
 kind version 1>/dev/null
-
-# ------------------------------------------------------------------------------
-# Setup Tools - KTF
-# ------------------------------------------------------------------------------
-
-# ensure ktf command is accessible
-if ! command -v ktf 1>/dev/null
-then
-    mkdir -p "${HOME}"/.local/bin
-    echo "Downloading KTF from ${KTF_URL}"
-    # grep location header to show the actual URL
-    curl -vL -o "${HOME}"/.local/bin/ktf "${KTF_URL}" 2>&1 | grep "location: https://github.com/Kong/kubernetes-testing-framework/releases/download/"
-    chmod +x "${HOME}"/.local/bin/ktf
-    export PATH="${HOME}/.local/bin:$PATH"
-fi
-
-# ensure kind is functional
-ktf 1>/dev/null
 
 # ------------------------------------------------------------------------------
 # Create Testing Environment
