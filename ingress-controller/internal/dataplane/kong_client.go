d3dfbf0f162cfec90a9878b1f6f98a4e9ad8c73d
Patryk Małek <patryk.malek@konghq.com>
2026-03-10 15:53:50 +0100

    fix: fix generating SNIs in dbless

--- ingress-controller/internal/dataplane/kong_client.go
+++ ingress-controller/internal/dataplane/kong_client.go
@@ -799,6 +799,7 @@ func (c *KongClient) sendToClient(
 		ExpressionRoutes:                config.ExpressionRoutes,
 		PluginSchemas:                   client.PluginSchemaStore(),
 		AppendStubEntityWhenConfigEmpty: config.InMemory,
+		InMemory:                        config.InMemory,
 	}
 	targetContent := deckgen.ToDeckContent(ctx, logger, s, deckGenParams)
 	customEntities := make(sendconfig.CustomEntitiesByType)
