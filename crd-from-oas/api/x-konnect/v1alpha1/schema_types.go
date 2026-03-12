91170c5343a26c14792170e4129a931956c0b6c6
Patryk Małek <patryk.malek@konghq.com>
2026-03-12 17:02:23 +0100

    feat: add event gateway CRD

--- crd-from-oas/api/x-konnect/v1alpha1/schema_types.go
+++ crd-from-oas/api/x-konnect/v1alpha1/schema_types.go
@@ -1,5 +1,18 @@
 package v1alpha1
 
+// GatewayDescription A human-readable description of the Gateway.
+type GatewayDescription string
+
+// GatewayName The name of the Gateway.
+type GatewayName string
+
+// Labels Labels store metadata of an entity that can be used for filtering an
+// entity list or for searching across entity types.
+//
+// Keys must be of length 1-63 characters, and cannot start with "kong",
+// "konnect", "mesh", "kic", or "_".
+type Labels map[string]string
+
 // LabelsUpdate Labels store metadata of an entity that can be used for
 // filtering an entity list or for searching across entity types.
 //
@@ -8,3 +21,10 @@ package v1alpha1
 // Keys must be of length 1-63 characters, and cannot start with "kong",
 // "konnect", "mesh", "kic", or "_".
 type LabelsUpdate map[string]string
+
+// MinRuntimeVersion The minimum runtime version supported by the API.
+// This is the lowest version of the data plane
+// release that can be used with the entity model.
+// When not specified, the minimum runtime version will be pinned to the latest
+// available release.
+type MinRuntimeVersion string
