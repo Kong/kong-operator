2ac176f13f07303b4007996db7115dbc315c1253
Patryk Małek <pmalek@users.noreply.github.com>
2026-03-17 13:26:00 +0100

    fix: fix requeue of KonnectAPIAuthConfiguration when there was an error calling organizations endpoint (#3611)
    (cherry picked from commit e6534dd5d1a8650c387e8f7ff650d768edbb7631)

--- controller/consts/requeue.go
+++ controller/consts/requeue.go
@@ -5,4 +5,9 @@ import "time"
 const (
 	// RequeueWithoutBackoff is the time after which the controller should requeue the request.
 	RequeueWithoutBackoff = time.Millisecond * 200
+
+	// RequeueWithBackoff is the time after which the controller should requeue the request with backoff.
+	// This is useful to avoid requeuing the request too frequently in case of
+	// e.g. external system errors.
+	RequeueWithBackoff = time.Second * 3
 )
