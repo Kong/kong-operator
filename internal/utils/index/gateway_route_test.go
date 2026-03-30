e0f5f3e7598e044028a74830e85e1a508810a595
Yi Tao <tao.yi@konghq.com>
2026-03-30 17:59:47 +0800

    use generics in index functions and add tests

--- internal/utils/index/gateway_route_test.go
+++ internal/utils/index/gateway_route_test.go
@@ -109,7 +109,7 @@ func TestGatewaysOnRoute_HTTPRoute(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := GatewaysOnRoute[gwtypes.HTTPRoute](tt.obj)
+			got := GatewaysOnRoute(tt.obj)
 			require.ElementsMatch(t, tt.want, got)
 		})
 	}
