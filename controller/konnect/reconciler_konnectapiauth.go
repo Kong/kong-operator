03779bc165a54c6c45d83471de59f61403bdb1ef
Tao Yi <tao.yi@konghq.com>
2026-05-11 15:48:01 +0800

    chore(logs): update the warning message for cross namespace ref for KonnectAPIAuth -> Secret (#4184)

--- controller/konnect/reconciler_konnectapiauth.go
+++ controller/konnect/reconciler_konnectapiauth.go
@@ -292,6 +292,8 @@ func GetTokenFromKonnectAPIAuthConfiguration(
 		if nn.Namespace != apiAuth.Namespace {
 			gvkAPIAuth := metav1.GroupVersionKind(apiAuth.GetObjectKind().GroupVersionKind())
 			gvkSecret := metav1.GroupVersionKind(corev1.SchemeGroupVersion.WithKind("Secret"))
+			// TODO: Require a KongReferenceGrant for cross-namespace references in KO 2.3 and later:
+			// https://github.com/Kong/kong-operator/issues/2908
 			err := crossnamespace.CheckKongReferenceGrantForResource(ctx, cl, apiAuth.Namespace, nn.Namespace, nn.Name, gvkAPIAuth, gvkSecret)
 			if err != nil {
 				ctrllog.FromContext(ctx).
@@ -300,7 +302,7 @@ func GetTokenFromKonnectAPIAuthConfiguration(
 							client.ObjectKeyFromObject(apiAuth), nn,
 						),
 						"WARNING: referencing Secret in a different namespace. "+
-							"This will require a KongReferenceGrant in Secret's namespace in future versions.",
+							"This will require a KongReferenceGrant in Secret's namespace in KO 2.3 and later.",
 					)
 			}
 		}
