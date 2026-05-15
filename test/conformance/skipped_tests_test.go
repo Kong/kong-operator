a8501d3bde1cff9139f5fe1e1774c76ef4da2fee
Yi Tao <tao.yi@konghq.com>
2026-05-15 14:24:01 +0800

    enable more conformance tests

--- test/conformance/skipped_tests_test.go
+++ test/conformance/skipped_tests_test.go
@@ -23,9 +23,6 @@ var skippedTestsShared = []string{
 
 	// TLSRoute tests that cannot pass yet.
 	tests.TLSRouteHostnameIntersection.ShortName,
-	tests.TLSRouteInvalidBackendRefNonexistent.ShortName,
-	tests.TLSRouteInvalidBackendRefUnknownKind.ShortName,
-	tests.TLSRouteInvalidNoMatchingListener.ShortName,
 	tests.TLSRouteListenerMixedTerminationNotSupported.ShortName,
 }
 
