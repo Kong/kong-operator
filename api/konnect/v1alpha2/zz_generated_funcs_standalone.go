f71b5a9115c5d911e35e28e6f8b35b119e2b27ec
Jintao Zhang <zhangjintao9020@gmail.com>
2026-05-19 23:12:55 +0800

    chore: refresh generated api helpers
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- api/konnect/v1alpha2/zz_generated_funcs_standalone.go
+++ api/konnect/v1alpha2/zz_generated_funcs_standalone.go
@@ -20,6 +20,7 @@ func (obj *KonnectGatewayControlPlane) GetKonnectID() string {
 func (obj *KonnectGatewayControlPlane) SetKonnectID(id string) {
 	obj.Status.ID = id
 }
+
 // PersistsKonnectID reports whether the KonnectGatewayControlPlane persists a Konnect ID in status.
 func (*KonnectGatewayControlPlane) PersistsKonnectID() bool {
 	return true
@@ -30,12 +31,12 @@ func (obj KonnectGatewayControlPlane) GetTypeName() string {
 	return "KonnectGatewayControlPlane"
 }
 
-// GetConditions returns the Status Conditions
+// GetConditions returns the Status Conditions.
 func (obj *KonnectGatewayControlPlane) GetConditions() []metav1.Condition {
 	return obj.Status.Conditions
 }
 
-// SetConditions sets the Status Conditions
+// SetConditions sets the Status Conditions.
 func (obj *KonnectGatewayControlPlane) SetConditions(conditions []metav1.Condition) {
 	obj.Status.Conditions = conditions
 }
