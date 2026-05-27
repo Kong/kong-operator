3d01d133bbf13a6a0ba50ecba2c03741423787d2
Jintao Zhang <zhangjintao9020@gmail.com>
2026-05-27 14:40:05 +0800

    test(conformance): enable HTTPRouteHeaderMatching for hybridgateway (#3917)
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- test/conformance/skipped_tests_test.go
+++ test/conformance/skipped_tests_test.go
@@ -39,7 +39,6 @@ var skippedTestsForHybrid = []string{
 	tests.HTTPRouteHTTPSListener.ShortName,
 	tests.HTTPRouteInvalidNonExistentBackendRef.ShortName,
 	tests.HTTPRouteListenerHostnameMatching.ShortName,
-	tests.HTTPRouteHeaderMatching.ShortName,
 	tests.HTTPRouteMethodMatching.ShortName,
 	tests.HTTPRouteMatchingAcrossRoutes.ShortName,
 	tests.HTTPRoutePathMatchOrder.ShortName,
