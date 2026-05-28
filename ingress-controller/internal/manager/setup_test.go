1a59724a0ed5268479632bb217c1c85bf7b199f8
Jakub Warczarek <jakub.warczarek@konghq.com>
2026-05-28 11:58:48 +0200

    Chore: adjust to new API

--- ingress-controller/internal/manager/setup_test.go
+++ ingress-controller/internal/manager/setup_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/avast/retry-go/v4"
+	"github.com/avast/retry-go/v5"
 	"github.com/go-logr/logr"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
