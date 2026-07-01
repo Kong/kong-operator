6773adb58d518fc6dc19f43cff32e1003daf70e2
Jintao Zhang <zhangjintao9020@gmail.com>
2026-07-01 18:01:35 +0800

    fix: preserve method precedence for hybrid header routes
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- test/conformance/skipped_tests_test.go
+++ test/conformance/skipped_tests_test.go
@@ -3,6 +3,7 @@ package conformance
 import (
 	"sigs.k8s.io/gateway-api/conformance/tests"
 
+	"github.com/kong/kong-operator/v2/pkg/consts"
 	"github.com/kong/kong-operator/v2/test"
 )
 
@@ -11,17 +12,30 @@ var skippedTestsShared = []string{}
 
 var skippedTestsForStandard = []string{}
 
+// skippedTestsForStandardTraditionalCompatibleRouter are ShortNames of tests
+// skipped only for the standard gateway with the traditional_compatible router
+// flavor.
+var skippedTestsForStandardTraditionalCompatibleRouter = []string{
+	// The standard gateway does not preserve Gateway API method-over-header
+	// match precedence with the traditional_compatible router flavor, so
+	// HTTPRouteMethodMatching is enabled for the hybrid gateway only.
+	tests.HTTPRouteMethodMatching.ShortName,
+}
+
 var skippedTestsForHybrid = []string{
 
 	// Extended profile.
 	tests.HTTPRouteQueryParamMatching.ShortName,
 }
 
-// skippedTestsForConfig returns the list of skipped tests for the given gateway type.
-func skippedTestsForConfig(gwType gatewayType) []string {
+// skippedTestsForConfig returns the list of skipped tests for the given router flavor and gateway type.
+func skippedTestsForConfig(routerFlavor consts.RouterFlavor, gwType gatewayType) []string {
 	skipped := append([]string{}, skippedTestsShared...)
 	if gwType == standardGateway {
 		skipped = append(skipped, skippedTestsForStandard...)
+		if routerFlavor == consts.RouterFlavorTraditionalCompatible {
+			skipped = append(skipped, skippedTestsForStandardTraditionalCompatibleRouter...)
+		}
 	}
 
 	if gwType == hybridGateway {
