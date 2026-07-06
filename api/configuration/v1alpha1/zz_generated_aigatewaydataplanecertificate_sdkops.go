22c08cbefebb5577b79730eca519c20ee59cc3cd
Patryk Małek <patryk.malek@konghq.com>
2026-07-06 16:16:00 +0200

    chore: make generate manifest

--- api/configuration/v1alpha1/zz_generated_aigatewaydataplanecertificate_sdkops.go
+++ api/configuration/v1alpha1/zz_generated_aigatewaydataplanecertificate_sdkops.go
@@ -12,7 +12,6 @@ import (
 
 	sdkkonnectcomp "github.com/Kong/sdk-konnect-go/models/components"
 )
-
 func (s *AIGatewayDataPlaneCertificateAPISpec) marshalSDKOpsPayload() ([]byte, error) {
 	data, err := json.Marshal(s)
 	if err != nil {
