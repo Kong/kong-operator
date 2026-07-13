0c7a629b4072e4c69423e11dffd237eb24117745
Patryk Małek <patryk.malek@konghq.com>
2026-07-13 10:44:31 +0200

    chore(ci): don't do a full checkout for docs-only

--- scripts/check-docs-only-changes.sh
+++ scripts/check-docs-only-changes.sh
@@ -7,12 +7,12 @@ CHANGED_FILES=$(git diff --name-only ${1:-$GITHUB_EVENT_PULL_REQUEST_BASE_SHA} $
 # Check if all changed files are documentation or license files
 DOCS_ONLY=true
 for file in $CHANGED_FILES; do
-  if [[ ! "$file" =~ ^(CHANGELOG\.md|README\.md|SECURITY\.md|FEATURES\.md|LICENSE|LICENSES|\.github/ISSUE_TEMPLATE/.*)$ ]]; then
+  if [[ ! "$file" =~ ^(LICENSE|LICENSES|\.github/ISSUE_TEMPLATE/.*|.*\.md)$ ]]; then
     DOCS_ONLY=false
     break
   fi
 done
 
-echo "docs_only=$DOCS_ONLY" >> $GITHUB_OUTPUT
+# echo "docs_only=$DOCS_ONLY" >> $GITHUB_OUTPUT
 echo "Changed files: $CHANGED_FILES"
 echo "Docs only: $DOCS_ONLY"
