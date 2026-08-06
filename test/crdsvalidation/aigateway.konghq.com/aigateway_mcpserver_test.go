56b6f104eae09353ac42f2d0e3c73848468e32fd
Yi Tao <tao.yi@konghq.com>
2026-08-06 15:52:54 +0800

    add crdsvalidation tests for aigateway ref immutable

---
+++ test/crdsvalidation/aigateway.konghq.com/aigateway_mcpserver_test.go
@@ -0,0 +1,54 @@
+package crdsvalidation_test
+
+import (
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	commonv1alpha1 "github.com/kong/kong-operator/v2/api/common/v1alpha1"
+	konnectv1alpha1 "github.com/kong/kong-operator/v2/api/konnect/v1alpha1"
+	"github.com/kong/kong-operator/v2/modules/manager/scheme"
+	"github.com/kong/kong-operator/v2/test/crdsvalidation/common"
+	"github.com/kong/kong-operator/v2/test/envtest"
+)
+
+func TestAIGatewayMCPServer(t *testing.T) {
+	t.Parallel()
+
+	ctx := t.Context()
+	scheme := scheme.Get()
+	cfg, ns := envtest.Setup(t, ctx, scheme)
+
+	t.Run("cp ref", func(t *testing.T) {
+		obj := &konnectv1alpha1.AIGatewayMCPServer{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "AIGatewayMCPServer",
+				APIVersion: konnectv1alpha1.GroupVersion.String(),
+			},
+			ObjectMeta: common.CommonObjectMeta(ns.Name),
+			Spec: konnectv1alpha1.AIGatewayMCPServerSpec{
+				AIGatewayRef: commonv1alpha1.ObjectRef{
+					Type: commonv1alpha1.ObjectRefTypeNamespacedRef,
+					NamespacedRef: &commonv1alpha1.NamespacedRef{
+						Name: "aigateway-1",
+					},
+				},
+				APISpec: konnectv1alpha1.AIGatewayMCPServerAPISpec{
+					AIGatewayMCPServerConfig: &konnectv1alpha1.AIGatewayMCPServerConfig{
+						Type: konnectv1alpha1.AIGatewayMCPServerConfigTypeListener,
+						Listener: &konnectv1alpha1.AIGatewayMCPServerListener{
+							Name:        "mcpserver1",
+							DisplayName: "Test MCP Server",
+							Config: konnectv1alpha1.AIGatewayMCPServerNoUpstreamConfig{
+								Route: konnectv1alpha1.AIGatewayRouteConfig{
+									Paths: []string{"/mcp"},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		common.NewCRDValidationTestCasesGroupParentRefChange(t, cfg, obj)
+	})
+}
---
+++ test/crdsvalidation/aigateway.konghq.com/aigateway_policy_test.go
@@ -0,0 +1,50 @@
+package crdsvalidation_test
+
+import (
+	"testing"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	commonv1alpha1 "github.com/kong/kong-operator/v2/api/common/v1alpha1"
+	konnectv1alpha1 "github.com/kong/kong-operator/v2/api/konnect/v1alpha1"
+	"github.com/kong/kong-operator/v2/modules/manager/scheme"
+	"github.com/kong/kong-operator/v2/test/crdsvalidation/common"
+	"github.com/kong/kong-operator/v2/test/envtest"
+)
+
+func TestAIGatewayPolicy(t *testing.T) {
+	t.Parallel()
+
+	ctx := t.Context()
+	scheme := scheme.Get()
+	cfg, ns := envtest.Setup(t, ctx, scheme)
+
+	t.Run("cp ref", func(t *testing.T) {
+		obj := &konnectv1alpha1.AIGatewayPolicy{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "AIGatewayPolicy",
+				APIVersion: konnectv1alpha1.GroupVersion.String(),
+			},
+			ObjectMeta: common.CommonObjectMeta(ns.Name),
+			Spec: konnectv1alpha1.AIGatewayPolicySpec{
+				AIGatewayRef: commonv1alpha1.ObjectRef{
+					Type: commonv1alpha1.ObjectRefTypeNamespacedRef,
+					NamespacedRef: &commonv1alpha1.NamespacedRef{
+						Name: "aigateway-1",
+					},
+				},
+				APISpec: konnectv1alpha1.AIGatewayPolicyAPISpec{
+					Name:        "policy1",
+					DisplayName: "Test Policy",
+					Type:        "ai-prompt-decorator",
+					Config: konnectv1alpha1.AIGatewayPolicyConfigDataSource{
+						Type:  "inline",
+						Value: &apiextensionsv1.JSON{Raw: []byte(`{}`)},
+					},
+				},
+			},
+		}
+		common.NewCRDValidationTestCasesGroupParentRefChange(t, cfg, obj)
+	})
+}
