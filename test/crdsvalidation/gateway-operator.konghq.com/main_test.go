8ce6cc556618a0242738d4a787029edd5d5cea5e
Patryk Małek <patryk.malek@konghq.com>
2026-08-12 15:15:05 +0200

    tests(crdsvalidation): share envtest setup to prevent resource contention and spawning too many etcd and kubeapiserver instances

---
+++ test/crdsvalidation/gateway-operator.konghq.com/main_test.go
@@ -0,0 +1,15 @@
+package crdsvalidation_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/kong/kong-operator/v2/test/envtest"
+)
+
+// TestMain shares a single envtest environment across every test in this
+// package instead of starting a fresh etcd+API server pair (and reinstalling
+// every CRD) per test. See envtest.RunWithSharedEnvironment.
+func TestMain(m *testing.M) {
+	os.Exit(envtest.RunWithSharedEnvironment(m))
+}
