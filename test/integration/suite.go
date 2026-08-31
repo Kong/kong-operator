e367a837954e12096653a99de9467c37d661e6ef
WeichengWang <hnwang643@gmail.com>
2026-08-31 15:40:24 +0800

    test(integration): extend integration tests for ipv6

--- test/integration/suite.go
+++ test/integration/suite.go
@@ -124,6 +124,8 @@ func Suite(m *testing.M, opts ...SuiteOption) {
 				b.WithIPv6Only()
 			case clusters.Dual:
 				exitOnErr(fmt.Errorf("dual-stack test clusters are not yet supported (KONG_TEST_CLUSTER_IP_FAMILY=dual)"))
+			case clusters.IPv4:
+				// KIND's default, nothing to configure.
 			}
 			// Calico's default manifest hardcodes an IPv4 pool CIDR, which breaks
 			// pod networking on an IPv6-only cluster. KIND's default CNI already
