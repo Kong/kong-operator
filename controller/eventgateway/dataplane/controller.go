7de9a01db5c295b9eb57ca2a664355a7c6f56e83
Jintao Zhang <zhangjintao9020@gmail.com>
2026-09-12 01:05:23 +0800

    chore: migrate EventGateway (KEG) DataPlane reconciler onto shared package
    Replace the EventGateway-specific DataPlane reconciler implementation with a
    thin wrapper that wires the shared generic reconciler from
    controller/pkg/dataplane via an EventGateway-specific Config. This completes
    the deduplication of the two near-identical specialized DataPlane reconcilers
    (AIGateway and EventGateway).
    
    Fixes #4842

--- controller/eventgateway/dataplane/controller.go
+++ controller/eventgateway/dataplane/controller.go
@@ -18,29 +18,18 @@ package dataplane
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"time"
 
-	appsv1 "k8s.io/api/apps/v1"
-	autoscalingv2 "k8s.io/api/autoscaling/v2"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/managedfields"
 	"k8s.io/client-go/tools/events"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	configurationv1alpha1 "github.com/kong/kong-operator/v2/api/configuration/v1alpha1"
 	eventgatewayv1alpha1 "github.com/kong/kong-operator/v2/api/eventgateway/v1alpha1"
 	konnectv1alpha1 "github.com/kong/kong-operator/v2/api/konnect/v1alpha1"
-	"github.com/kong/kong-operator/v2/controller/pkg/address"
-	log "github.com/kong/kong-operator/v2/controller/pkg/log"
-	"github.com/kong/kong-operator/v2/controller/pkg/op"
+	shareddataplane "github.com/kong/kong-operator/v2/controller/pkg/dataplane"
 	"github.com/kong/kong-operator/v2/modules/manager/logging"
-	k8sutils "github.com/kong/kong-operator/v2/pkg/utils/kubernetes"
 )
 
 // Reconciler reconciles a KegDataPlane object.
@@ -64,153 +53,40 @@ type Reconciler struct {
 	eventRecorder events.EventRecorder
 }
 
+// base returns the shared generic reconciler wired with the KegDataPlane
+// configuration.
+func (r *Reconciler) base() *shareddataplane.Reconciler[
+	*eventgatewayv1alpha1.KegDataPlane,
+	*konnectv1alpha1.KonnectEventGateway,
+	*configurationv1alpha1.EventGatewayDataPlaneCertificate,
+] {
+	return &shareddataplane.Reconciler[
+		*eventgatewayv1alpha1.KegDataPlane,
+		*konnectv1alpha1.KonnectEventGateway,
+		*configurationv1alpha1.EventGatewayDataPlaneCertificate,
+	]{
+		Client:                   r.Client,
+		LoggingMode:              r.LoggingMode,
+		ClusterCASecretName:      r.ClusterCASecretName,
+		ClusterCASecretNamespace: r.ClusterCASecretNamespace,
+		SecretLabelSelector:      r.SecretLabelSelector,
+		CertTTL:                  r.CertTTL,
+		TypeConverter:            r.TypeConverter,
+		EventRecorder:            r.eventRecorder,
+		Config:                   config,
+	}
+}
+
 // SetupWithManager sets up the controller with the Manager.
 func (r *Reconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) error {
 	r.eventRecorder = mgr.GetEventRecorder(ControllerName)
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&eventgatewayv1alpha1.KegDataPlane{}).
-		Owns(&appsv1.Deployment{}).
-		Owns(&autoscalingv2.HorizontalPodAutoscaler{}).
-		Owns(&corev1.Service{}).
-		Owns(&corev1.Secret{}).
-		Owns(&configurationv1alpha1.EventGatewayDataPlaneCertificate{}).
-		Watches(
-			&konnectv1alpha1.KonnectEventGateway{},
-			handler.EnqueueRequestsFromMapFunc(enqueueForKonnectEventGatewayRef(mgr.GetClient())),
-		).
-		Complete(reconcile.AsReconciler[*eventgatewayv1alpha1.KegDataPlane](r.Client, r))
+	return r.base().SetupWithManager(ctx, mgr)
 }
 
 // Reconcile moves the current state of a KegDataPlane toward the desired state.
-func (r *Reconciler) Reconcile(ctx context.Context, egdp *eventgatewayv1alpha1.KegDataPlane) (res ctrl.Result, err error) {
-	logger := log.GetLogger(ctx, "keg-dataplane", r.LoggingMode)
-
-	log.Trace(logger, "reconciling KegDataPlane resource")
-
-	defer func() {
-		err = errors.Join(err, ensureReadyStatus(ctx, r.Client, egdp))
-		err = errors.Join(err, r.applyStatus(ctx, logger, egdp))
-	}()
-
-	// Resolve referenced KonnectEventGateway and set resolution condition.
-	keg, err := r.resolveKonnectEventGateway(ctx, logger, egdp)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	// Ensure mTLS client certificate secret and set certificate condition.
-	certResult, certSecret, err := r.ensureCertificateSecret(ctx, egdp)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	// Return early if the Secret was just created/updated so the Deployment
-	// picks up the correct Secret name on the next reconcile. No explicit
-	// requeue is needed, the watch on the owned Secret triggers it.
-	if certResult != op.Noop {
-		return ctrl.Result{}, nil
-	}
-
-	// Ensure the EventGatewayDataPlaneCertificate is registered with Konnect.
-	// Return early if not yet programmed; the Owns() watch retriggeres once
-	// the Konnect controller flips Programmed to True.
-	certProgrammed, err := r.ensureKonnectCertificate(ctx, logger, egdp, keg, certSecret)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-	// If the certificate is not yet programmed on Konnect, return early.
-	// Without this, we would create a deployment that uses a cert secret not yet present in Konnect.
-	if !certProgrammed {
-		return ctrl.Result{}, nil
-	}
-
-	// Reconcile the full Keg Deployment spec.
-	if err := r.ensureDeployment(ctx, logger, egdp, keg, certSecret.Name); err != nil {
-		return ctrl.Result{}, err
-	}
-
-	// Reconcile the HPA if horizontal scaling is configured.
-	if err := r.ensureHPA(ctx, logger, egdp, egdp.Name); err != nil {
-		return ctrl.Result{}, err
-	}
-
-	// Ensure the Kafka Service and set its readiness condition.
-	// nil svc means the cache hasn't caught up yet; the Owns() watch will
-	// trigger another reconcile once the Service appears.
-	svc, err := r.ensureKafkaService(ctx, logger, egdp)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-	if svc != nil {
-		if err := r.ensureServiceReadyCondition(egdp, svc); err != nil {
-			return ctrl.Result{}, err
-		}
-	}
-
-	log.Debug(logger, "reconciliation complete for DataPlane resource")
-	return ctrl.Result{}, nil
-}
-
-// ensureServiceReadyCondition sets the ServiceReady condition and populates
-// Status.Addresses based on the live Kafka Service.
-func (r *Reconciler) ensureServiceReadyCondition(
+func (r *Reconciler) Reconcile(
+	ctx context.Context,
 	egdp *eventgatewayv1alpha1.KegDataPlane,
-	svc *corev1.Service,
-) error {
-	svcAddrs, err := address.AddressesFromService(svc)
-	if err != nil {
-		return fmt.Errorf("failed to get addresses from Kafka Service for KegDataPlane %s/%s: %w",
-			egdp.Namespace, egdp.Name, err)
-	}
-	addrs := make([]eventgatewayv1alpha1.Address, len(svcAddrs))
-	for i, a := range svcAddrs {
-		addrs[i] = eventgatewayv1alpha1.Address{
-			Value:      a.Value,
-			SourceType: eventgatewayv1alpha1.AddressSourceType(a.SourceType),
-		}
-		if a.Type != nil {
-			t := eventgatewayv1alpha1.AddressType(*a.Type)
-			addrs[i].Type = &t
-		}
-	}
-	egdp.Status.Addresses = addrs
-
-	if kafkaServiceIsReady(svc) {
-		k8sutils.SetCondition(
-			k8sutils.NewConditionWithGeneration(
-				eventgatewayv1alpha1.ServiceReadyType,
-				metav1.ConditionTrue,
-				eventgatewayv1alpha1.ServiceReadyReason,
-				eventgatewayv1alpha1.ServiceReadyMessage,
-				egdp.Generation,
-			),
-			egdp,
-		)
-	} else {
-		k8sutils.SetCondition(
-			k8sutils.NewConditionWithGeneration(
-				eventgatewayv1alpha1.ServiceReadyType,
-				metav1.ConditionFalse,
-				eventgatewayv1alpha1.WaitingForAddressReason,
-				eventgatewayv1alpha1.WaitingForAddressMessage,
-				egdp.Generation,
-			),
-			egdp,
-		)
-	}
-	return nil
-}
-
-// kafkaServiceIsReady reports whether the Kafka Service has an external
-// address. Non-LoadBalancer Services are always considered ready.
-func kafkaServiceIsReady(svc *corev1.Service) bool {
-	if svc.Spec.Type != corev1.ServiceTypeLoadBalancer {
-		return true
-	}
-	for _, ing := range svc.Status.LoadBalancer.Ingress {
-		if ing.Hostname != "" || ing.IP != "" {
-			return true
-		}
-	}
-	return false
+) (ctrl.Result, error) {
+	return r.base().Reconcile(ctx, egdp)
 }
