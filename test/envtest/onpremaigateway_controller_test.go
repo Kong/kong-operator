0075bbd7e96503e7feade9b7161e9e3733e8187b
WeichengWang <hnwang643@gmail.com>
2026-09-22 16:38:53 +0800

    add Group & Type into AIGatewayRef in envtest

--- test/envtest/onpremaigateway_controller_test.go
+++ test/envtest/onpremaigateway_controller_test.go
@@ -156,6 +156,7 @@ func TestOnPremAIGatewayReconciler_ConfigTracksAIGatewayModels(t *testing.T) {
 		Spec: aiconfigurationv1alpha1.AIGatewayModelProviderSpec{
 			AIGatewayRef: aiconfigurationv1alpha1.AIGatewayRef{
 				Kind:          aiconfigurationv1alpha1.AIGatewayRefKindOnPrem,
+				Group:         aiconfigurationv1alpha1.AIGatewayRefGroupOnPrem,
 				NamespacedRef: &commonv1alpha1.NamespacedRef{Name: onprem.Name},
 			},
 			APISpec: aiconfigurationv1alpha1.AIGatewayModelProviderAPISpec{
@@ -184,9 +185,11 @@ func TestOnPremAIGatewayReconciler_ConfigTracksAIGatewayModels(t *testing.T) {
 		Namespace: ns.Name,
 		Spec: aiconfigurationv1alpha1.AIGatewayModelSpec{
 			AIGatewayRef: aiconfigurationv1alpha1.AIGatewayRef{
-				Kind: aiconfigurationv1alpha1.AIGatewayRefKindOnPrem,
-				NamespacedRef: &commonv1alpha1.NamespacedRef{
-					Name: onprem.Name},
+				//nolint:staticcheck
+				Type:          aiconfigurationv1alpha1.AIGatewayRefTypeNamespacedRef,
+				Kind:          aiconfigurationv1alpha1.AIGatewayRefKindOnPrem,
+				Group:         aiconfigurationv1alpha1.AIGatewayRefGroupOnPrem,
+				NamespacedRef: &commonv1alpha1.NamespacedRef{Name: onprem.Name},
 			},
 			APISpec: aiconfigurationv1alpha1.AIGatewayModelAPISpec{
 				AIGatewayModelConfig: &aiconfigurationv1alpha1.AIGatewayModelConfig{
